d2113e79af5852aa30b76c82a6cda5048f421c27
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-07-08 10:30:45 +0700

    refactor: remove src directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {},
   plugins: [],
 };
