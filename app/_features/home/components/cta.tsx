0244828767bafa3ed18a5f34c609518fc66e1f3a
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-07-10 21:03:38 +0700

    feat: add home cta components

--- app/_features/home/components/cta.tsx
+++ app/_features/home/components/cta.tsx
@@ -1,4 +1,24 @@
+'use client';
+
+import { createRoom, registerClient } from '@/_features/room/modules/factory';
+import { useRouter } from 'next/navigation';
+
 export default function HomeCTA() {
+  const router = useRouter();
+
+  const handleCreateRoom = async () => {
+    try {
+      const room = await createRoom();
+      const client = await registerClient(room.roomId);
+      window.localStorage.setItem('clientId', client.clientId);
+      router.push(`/room/${room.roomId}`);
+      router.refresh();
+    } catch (error) {
+      alert('Something went wrong. Please try again later! ');
+      console.error(error);
+    }
+  };
+
   return (
     <section className="max-w-xl lg:max-w-lg">
       <h2 className="text-3xl font-bold tracking-wide lg:text-4xl">
@@ -9,7 +29,10 @@ export default function HomeCTA() {
         started now by creating a room or join to other rooms with room code.
       </p>
       <div className="mt-8">
-        <button className="w-full rounded-md border border-red-700 bg-red-700 px-6 py-2 text-sm font-medium lg:w-auto">
+        <button
+          className="w-full rounded-md border border-red-700 bg-red-700 px-6 py-2 text-sm font-medium lg:w-auto"
+          onClick={handleCreateRoom}
+        >
           Create a new room
         </button>
       </div>
