1413786fbc5ed724fa983ce5ac379694144cc679
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-07-10 21:06:06 +0700

    fix: ignore explicit any eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,9 @@ module.exports = {
       parser: '@typescript-eslint/parser',
       plugins: ['@typescript-eslint/eslint-plugin'],
       extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
     },
   ],
 };
