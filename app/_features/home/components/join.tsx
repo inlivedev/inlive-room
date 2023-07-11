ee8d2c5ac3ff2878fe203a5367a38a4fed8c8ab9
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-07-11 10:05:03 +0700

    feat: enable user to join room

--- app/_features/home/components/join.tsx
+++ app/_features/home/components/join.tsx
@@ -1,4 +1,32 @@
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { registerClient } from '@/_features/room/modules/factory';
+
 export default function HomeJoin() {
+  const router = useRouter();
+  const [roomId, setRoomId] = useState<string>('');
+
+  const handleInputRoomIdChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRoomId(event.target.value);
+  };
+
+  const handleJoinRoom = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    try {
+      const client = await registerClient(roomId);
+      window.localStorage.setItem('clientId', client.clientId);
+      router.push(`/room/${roomId}`);
+      router.refresh();
+    } catch (error) {
+      alert('Something went wrong. Please try again later! ');
+      console.error(error);
+    }
+  };
+
   return (
     <form className="w-full max-w-xl rounded-md bg-neutral-800 p-6 shadow-md">
       <h3 className="text-xl font-semibold tracking-wide">Join a room</h3>
@@ -10,8 +38,11 @@ export default function HomeJoin() {
           type="text"
           className="flex-1 rounded-md border border-neutral-600 bg-neutral-800 px-4 py-2 text-sm outline-none placeholder:text-neutral-500 focus:border-neutral-500"
           placeholder="Enter a room code"
+          onChange={handleInputRoomIdChange}
         />
-        <button className="text-sm font-medium">Join</button>
+        <button className="text-sm font-medium" onClick={handleJoinRoom}>
+          Join
+        </button>
       </div>
     </form>
   );
