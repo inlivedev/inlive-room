f66b16df0ee3abfa2d080e699cf13565c9012241
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-07-14 08:46:56 +0700

    Revert "refactor: room module refactor"
    This reverts commit f1f9660421b5dc113f48f0030e2c6e7c4d5bc8ca.

--- app/_features/room/modules/room.ts
+++ app/_features/room/modules/room.ts
@@ -54,48 +54,129 @@ export class Room {
   }
 
   disconnect() {
-    this.#closeConnection();
-    this.#removeListener();
-  }
+    if (this.#peerConnection) {
+      const localStream = this.media.getLocalStream();
 
-  on(eventName: string, callback: (event: any) => any) {
-    this.#event.on(eventName, callback);
-  }
+      for (const track of localStream.getTracks()) {
+        track.stop();
+      }
 
-  getPeerConnection() {
-    return this.#peerConnection;
+      for (const sender of this.#peerConnection.getSenders()) {
+        this.#peerConnection.removeTrack(sender);
+      }
+
+      this.#peerConnection.close();
+      this.#peerConnection = null;
+    }
   }
 
   #addListener() {
-    window.addEventListener('beforeunload', this.#beforeUnloadHandler);
+    window.addEventListener('beforeunload', (event) => {
+      event.preventDefault();
+
+      (() => {
+        this.disconnect();
+        leaveRoom(this.#roomId, this.#clientId);
+      })();
+    });
 
     if (!this.#peerConnection) return;
 
-    this.#peerConnection.addEventListener(
-      'iceconnectionstatechange',
-      this.#peerIceconnectionStateChangeHandler
-    );
-    this.#peerConnection.addEventListener('track', this.#peerTrackHandler);
+    this.#peerConnection.addEventListener('iceconnectionstatechange', () => {
+      if (this.#peerConnection) {
+        console.log(
+          'ice connection state changed to',
+          this.#peerConnection.iceConnectionState
+        );
+      }
+    });
+
+    this.#peerConnection.addEventListener('track', (event) => {
+      const stream = event.streams.find((stream) => stream.active);
+
+      if (!(stream instanceof MediaStream)) return;
+
+      stream.addEventListener('removetrack', (event) => {
+        const target = event.target;
+
+        if (target instanceof MediaStream) {
+          const remoteStreams = this.media.getRemoteStreams();
+
+          if (target.id in remoteStreams && target.getTracks().length === 0) {
+            const stream = target;
+            this.media.removeStream(target.id);
+            this.#event.emit(Room.PARTICIPANT_REMOVED, {
+              stream: stream,
+            });
+          }
+        }
+      });
+
+      const remoteStreams = this.media.getRemoteStreams();
+      if (remoteStreams[stream.id] === undefined) {
+        this.addStream(stream, 'remote');
+      }
+    });
+
     this.#peerConnection.addEventListener(
       'icecandidate',
-      this.#peerIceCandidateHandler
+      async ({ candidate }) => {
+        if (candidate) {
+          sendIceCandidate(this.#roomId, this.#clientId, candidate);
+        }
+      }
     );
-    this.#peerConnection.addEventListener(
-      'negotiationneeded',
-      this.#peerNegotiationNeededHandler
-    );
-    this.#channel.addEventListener('candidate', this.#channelCandidateHandler);
-    this.#channel.addEventListener('offer', this.#channelOfferHandler);
+
+    this.#peerConnection.addEventListener('negotiationneeded', async () => {
+      if (!this.#peerConnection) return;
+
+      const offer = await this.#peerConnection.createOffer();
+      await this.#peerConnection.setLocalDescription(offer);
+
+      const response = await joinRoom(
+        this.#roomId,
+        this.#clientId,
+        this.#peerConnection.localDescription
+      );
+
+      const data = response.data || {};
+      const answer = data.answer;
+      const sdpAnswer = new RTCSessionDescription(answer);
+      await this.#peerConnection.setRemoteDescription(sdpAnswer);
+    });
+
+    this.#channel.addEventListener('candidate', (event) => {
+      if (!this.#peerConnection || !this.#peerConnection.remoteDescription) {
+        return;
+      }
+
+      const candidate = new RTCIceCandidate(JSON.parse(event.data));
+      this.#peerConnection.addIceCandidate(candidate);
+    });
+
+    this.#channel.addEventListener('offer', async (event) => {
+      if (!this.#peerConnection) return;
+
+      const offer = JSON.parse(event.data);
+      await this.#peerConnection.setRemoteDescription(offer);
+      const answer = await this.#peerConnection.createAnswer();
+
+      await this.#peerConnection.setLocalDescription(answer);
+
+      renegotiatePeer(
+        this.#roomId,
+        this.#clientId,
+        this.#peerConnection.localDescription
+      );
+    });
   }
 
-  #removeListener() {
-    window.removeEventListener('beforeunload', this.#beforeUnloadHandler);
-  }
-
-  #beforeUnloadHandler(event: BeforeUnloadEvent) {
-    event.preventDefault();
-    this.disconnect();
-    leaveRoom(this.#roomId, this.#clientId);
+  addStream(stream: MediaStream, type: string) {
+    this.media.addStream(stream);
+    this.#event.emit(Room.PARTICIPANT_ADDED, {
+      stream: stream,
+      type: type,
+    });
   }
 
   #addLocalTrack() {
@@ -113,120 +194,11 @@ export class Room {
     });
   }
 
-  #addParticipant(stream: MediaStream, type: string) {
-    this.media.addStream(stream);
-    this.#event.emit(Room.PARTICIPANT_ADDED, {
-      stream: stream,
-      type: type,
-    });
+  on(eventName: string, callback: (event: any) => any) {
+    this.#event.on(eventName, callback);
   }
 
-  #channelCandidateHandler(event: MessageEvent<any>) {
-    if (!this.#peerConnection || !this.#peerConnection.remoteDescription) {
-      return;
-    }
-
-    const candidate = new RTCIceCandidate(JSON.parse(event.data));
-    this.#peerConnection.addIceCandidate(candidate);
-  }
-
-  #channelOfferHandler(event: MessageEvent<any>) {
-    (async (event) => {
-      if (!this.#peerConnection) return;
-
-      const offer = JSON.parse(event.data);
-      await this.#peerConnection.setRemoteDescription(offer);
-      const answer = await this.#peerConnection.createAnswer();
-
-      await this.#peerConnection.setLocalDescription(answer);
-
-      renegotiatePeer(
-        this.#roomId,
-        this.#clientId,
-        this.#peerConnection.localDescription
-      );
-    })(event);
-  }
-
-  #peerIceconnectionStateChangeHandler() {
-    if (this.#peerConnection) {
-      console.log(
-        'ice connection state changed to',
-        this.#peerConnection.iceConnectionState
-      );
-    }
-  }
-
-  #peerTrackHandler(event: RTCTrackEvent) {
-    const stream = event.streams.find((stream) => stream.active);
-
-    if (!(stream instanceof MediaStream)) return;
-
-    stream.addEventListener('removetrack', (event) => {
-      const target = event.target;
-
-      if (target instanceof MediaStream) {
-        const remoteStreams = this.media.getRemoteStreams();
-
-        if (target.id in remoteStreams && target.getTracks().length === 0) {
-          const stream = target;
-          this.media.removeStream(target.id);
-          this.#event.emit(Room.PARTICIPANT_REMOVED, {
-            stream: stream,
-          });
-        }
-      }
-    });
-
-    const remoteStreams = this.media.getRemoteStreams();
-
-    if (remoteStreams[stream.id] === undefined) {
-      this.#addParticipant(stream, 'remote');
-    }
-  }
-
-  #peerIceCandidateHandler(event: RTCPeerConnectionIceEvent) {
-    (async ({ candidate }) => {
-      if (candidate) {
-        sendIceCandidate(this.#roomId, this.#clientId, candidate);
-      }
-    })(event);
-  }
-
-  #peerNegotiationNeededHandler() {
-    (async () => {
-      if (!this.#peerConnection) return;
-
-      const offer = await this.#peerConnection.createOffer();
-      await this.#peerConnection.setLocalDescription(offer);
-
-      const response = await joinRoom(
-        this.#roomId,
-        this.#clientId,
-        this.#peerConnection.localDescription
-      );
-
-      const data = response.data || {};
-      const answer = data.answer;
-      const sdpAnswer = new RTCSessionDescription(answer);
-      await this.#peerConnection.setRemoteDescription(sdpAnswer);
-    })();
-  }
-
-  #closeConnection() {
-    if (this.#peerConnection) {
-      const localStream = this.media.getLocalStream();
-
-      for (const track of localStream.getTracks()) {
-        track.stop();
-      }
-
-      for (const sender of this.#peerConnection.getSenders()) {
-        this.#peerConnection.removeTrack(sender);
-      }
-
-      this.#peerConnection.close();
-      this.#peerConnection = null;
-    }
+  getPeerConnection() {
+    return this.#peerConnection;
   }
 }
