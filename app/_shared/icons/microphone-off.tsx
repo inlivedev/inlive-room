7a05eced51403b1d7d978ac0a1992415a492fb10
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-07-20 14:59:21 +0700

    refactor: rename file

--- app/_shared/icons/microphone-off.tsx
+++ app/_shared/icons/microphone-off.tsx
@@ -1,6 +1,6 @@
 import type { SVGElementPropsType } from '@/_shared/types/types.d.ts';
 
-export default function MicOff(props: SVGElementPropsType) {
+export default function MicrophoneOff(props: SVGElementPropsType) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
