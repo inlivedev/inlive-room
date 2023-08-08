132080ade5784507e9c2911085d18baca6400dfa
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-08-08 16:13:48 +0700

    refactor: use metadata from each page instead

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,6 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        <meta name="title" content="" />
-        <meta name="description" content="" />
         <link rel="icon" href="/images/favicon/favicon.ico" sizes="any" />
         <link
           rel="icon"
