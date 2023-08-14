67592018f9497f9154f9013843dff2b940ae99a9
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-08-14 17:23:00 +0700

    feat: sentry integration (#24)
    * feat: setup sentry and configuration
    
    * feat: add some sentry files to .gitignore
    
    * feat: add sentry dsn env
    
    * feat: add error boundary component
    
    * fix: disable debug as default
    
    * feat: enable tracesSampleRate on 0.1 rate

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+const { withSentryConfig } = require('@sentry/nextjs');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
 };
 
-module.exports = nextConfig;
+module.exports = withSentryConfig(
+  nextConfig,
+  {
+    silent: true,
+  },
+  {
+    hideSourceMaps: true,
+    disableLogger: true,
+    widenClientFileUpload: true,
+  }
+);
---
+++ sentry.client.config.ts
@@ -0,0 +1,13 @@
+import * as Sentry from "@sentry/nextjs";
+
+const APP_ENV = process.env.NEXT_PUBLIC_APP_ENV;
+const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+if (SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    environment: APP_ENV,
+    debug: false,
+    tracesSampleRate: 0.1
+  });
+}
