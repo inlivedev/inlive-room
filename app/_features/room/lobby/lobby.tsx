3f7c71e3d21746c4b8ab9da0ea3099e258d678af
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-08-25 06:36:42 +0700

    style: remove class

--- app/_features/room/lobby/lobby.tsx
+++ app/_features/room/lobby/lobby.tsx
@@ -7,7 +7,7 @@ type LobbyProps = {
 
 export default function Lobby({ children }: LobbyProps) {
   return (
-    <div className="flex min-h-screen flex-1 flex-col">
+    <div className="flex min-h-screen flex-col">
       <div className="mx-auto flex w-full max-w-xl flex-1 flex-col gap-10 px-4">
         <Header />
         <main className="flex flex-1 flex-col justify-center">
