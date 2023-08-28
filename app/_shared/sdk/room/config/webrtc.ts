de6ca9cc558c50495febbb9b68b2683c32b7dbd0
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-08-28 13:46:58 +0700

    feat: room sdk config

---
+++ app/_shared/sdk/room/config/webrtc.ts
@@ -0,0 +1,10 @@
+export const iceServers = [
+  {
+    urls: 'turn:turn.inlive.app:3478',
+    username: 'inlive',
+    credential: 'inlivesdkturn',
+  },
+  {
+    urls: 'stun:turn.inlive.app:3478',
+  },
+];
