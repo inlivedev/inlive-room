a611706352553f141e90912a902986e88fc59663
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-08-28 16:18:56 +0700

    fix: get the first protocol from x-forwarded-proto

--- app/_shared/utils/get-origin-server-side.ts
+++ app/_shared/utils/get-origin-server-side.ts
@@ -2,7 +2,8 @@ import { headers } from 'next/headers';
 
 export const getOriginServerSide = () => {
   const headersList = headers();
-  const protocol = headersList.get('x-forwarded-proto') || 'http';
+  const protocol =
+    headersList.get('x-forwarded-proto')?.split(',')[0] || 'http';
   const host = headersList.get('x-forwarded-host') || headersList.get('host');
   const origin = `${protocol}://${host}`;
   return origin;
