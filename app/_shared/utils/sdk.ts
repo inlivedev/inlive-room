40919402f33b230eafac571a09fed3a685f9c21d
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-08-28 15:21:10 +0700

    refactor: refactor room sdk initialization

--- app/_shared/utils/sdk.ts
+++ app/_shared/utils/sdk.ts
@@ -1,3 +1,10 @@
 import { Room } from '@/_shared/sdk/room';
+const hubOrigin = process.env.NEXT_PUBLIC_HUB_ORIGIN || '';
+const apiVersion = process.env.NEXT_PUBLIC_API_VERSION || '';
 
-export const room = Room();
+export const room = Room({
+  api: {
+    baseUrl: hubOrigin,
+    version: apiVersion,
+  },
+});
