bcdf40c632a3d440f4aa030894bd5198279476df
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-08-29 09:29:03 +0700

    refactor: participant use class and factory function

--- app/_shared/sdk/room/participant/participant.ts
+++ app/_shared/sdk/room/participant/participant.ts
@@ -1,29 +1,53 @@
-export const Participant = () => {
-  const participants = new Map<string, RoomParticipantTypes.Participant>();
+class Participant {
+  _participants;
 
+  constructor() {
+    this._participants = new Map<string, RoomParticipantTypes.Participant>();
+  }
+
+  addParticipant(key: string, value: RoomParticipantTypes.Participant) {
+    if (key.trim().length > 0) {
+      this._participants.set(key, value);
+    }
+  }
+
+  removeParticipant(key: string) {
+    if (key.trim().length > 0) {
+      this._participants.delete(key);
+    }
+  }
+
+  getAllParticipants() {
+    return [...this._participants.values()];
+  }
+
+  getParticipant(key: string) {
+    return this._participants.get(key);
+  }
+
+  getTotalParticipants() {
+    return this._participants.size;
+  }
+}
+
+export const participantFactory = () => {
   return {
-    addParticipant(key: string, value: RoomParticipantTypes.Participant) {
-      if (key.trim().length > 0) {
-        participants.set(key, value);
-      }
-    },
+    create() {
+      const {
+        addParticipant,
+        removeParticipant,
+        getAllParticipants,
+        getParticipant,
+        getTotalParticipants,
+      } = new Participant();
 
-    removeParticipant(key: string) {
-      if (key.trim().length > 0) {
-        participants.delete(key);
-      }
-    },
-
-    getAllParticipants() {
-      return [...participants.values()];
-    },
-
-    getParticipant(key: string) {
-      return participants.get(key);
-    },
-
-    getTotalParticipants() {
-      return participants.size;
+      return {
+        addParticipant,
+        removeParticipant,
+        getAllParticipants,
+        getParticipant,
+        getTotalParticipants,
+      };
     },
   };
 };
