6baf92361bab79ef80c3676ef747403cbda98a20
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-08-29 12:56:41 +0700

    feat: expose more sdk modules
    expose more sdk modules to client
    - createPeer
    - leaveRoom
    - terminateRoom

--- app/_shared/sdk/room/index.ts
+++ app/_shared/sdk/room/index.ts
@@ -1,17 +1,41 @@
-import { createRoom, getRoom, registerClient } from './api/api';
+import { apiFactory } from './api/api';
+import { channelFactory } from './channel/channel';
+import { eventFactory } from './event/event';
+import { peerFactory } from './peer/peer';
+import { participantFactory } from './participant/participant';
 
-const factories = {
-  createRoom,
-  createClient: registerClient,
-  getRoom,
-};
+export const Room = (config: RoomTypes.Config) => {
+  const baseUrl = `${config.api.baseUrl}/${config.api.version}`;
+  const api = apiFactory(baseUrl);
+  const event = eventFactory();
+  const participant = participantFactory();
 
-export const Room = (({ createRoom, createClient, getRoom }) => {
-  return () => {
-    return {
-      createRoom,
-      createClient,
-      getRoom,
-    };
+  const peer = peerFactory({
+    api,
+    event,
+    participant,
+  });
+
+  const channel = channelFactory({
+    baseUrl,
+    api,
+    peer,
+  });
+
+  const { connect: connectToPeer, ...restOfPeer } = peer;
+  const { connect: connectToChannel } = channel;
+
+  return {
+    createRoom: api.createRoom,
+    createClient: api.registerClient,
+    getRoom: api.getRoom,
+    createPeer: (roomId = '', clientId = '') => {
+      connectToPeer(roomId, clientId);
+      connectToChannel(roomId, clientId);
+
+      return restOfPeer;
+    },
+    leaveRoom: api.leaveRoom,
+    terminateRoom: api.terminateRoom,
   };
-})(factories);
+};
