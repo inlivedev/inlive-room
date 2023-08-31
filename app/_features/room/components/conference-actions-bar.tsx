64c2ceececc17e6eba2dc905d916d2d2b913682a
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-08-31 18:16:30 +0700

    feat: camera, mic, and screen share buttons

--- app/_features/room/components/conference-actions-bar.tsx
+++ app/_features/room/components/conference-actions-bar.tsx
@@ -1,8 +1,20 @@
 import ButtonLeave from '@/_features/room/components/button-leave';
+import ButtonToggleMicrophone from '@/_features/room/components/button-toggle-microphone';
+import ButtonToggleCamera from '@/_features/room/components/button-toggle-camera';
+import ButtonScreenShare from '@/_features/room/components/button-screen-share';
 
 export default function ConferenceActionsBar() {
   return (
     <div className="flex justify-center gap-3">
+      <div>
+        <ButtonToggleMicrophone />
+      </div>
+      <div>
+        <ButtonToggleCamera />
+      </div>
+      <div>
+        <ButtonScreenShare />
+      </div>
       <div>
         <ButtonLeave />
       </div>
