24156f83c94fc40351ad64c04f237dad01e6657c
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-08-31 17:49:32 +0700

    feat: import conference participant and action bar

--- app/_features/room/components/conference.tsx
+++ app/_features/room/components/conference.tsx
@@ -1,18 +1,17 @@
-import RoomContainer from '@/_features/room/components/room-container';
-import { ParticipantProvider } from '@/_features/room/contexts/participant-context';
+import ConferenceParticipants from '@/_features/room/components/conference-participants';
+import ConferenceActionsBar from '@/_features/room/components/conference-actions-bar';
 
-export default function Conference({
-  roomId,
-  clientId,
-  mediaStream,
-}: {
-  roomId: string;
-  clientId: string;
-  mediaStream: MediaStream;
-}) {
+export default function Conference() {
   return (
-    <ParticipantProvider mediaStream={mediaStream}>
-      <RoomContainer roomId={roomId} clientId={clientId} />
-    </ParticipantProvider>
+    <div className="flex min-h-screen flex-col">
+      <div className="mx-auto flex w-full max-w-7xl flex-1 flex-col">
+        <div className={`grid flex-1 grid-cols-1 p-4`}>
+          <ConferenceParticipants />
+        </div>
+        <div className="p-4">
+          <ConferenceActionsBar />
+        </div>
+      </div>
+    </div>
   );
 }
