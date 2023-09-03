8f1cfa488206b68d6ddacad01ba003884634186c
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-03 19:46:39 +0700

    refactor: log only error and default value constraints

--- app/_features/room/hooks/use-screen-share.ts
+++ app/_features/room/hooks/use-screen-share.ts
@@ -3,7 +3,7 @@ import { usePeerContext } from '@/_features/room/contexts/peer-context';
 export const useScreenShare = () => {
   const { peer } = usePeerContext();
 
-  const screenShare = async (mediaConstraints: MediaStreamConstraints) => {
+  const screenShare = async (mediaConstraints: MediaStreamConstraints = {}) => {
     try {
       if (!peer) return;
 
@@ -18,15 +18,6 @@ export const useScreenShare = () => {
       });
     } catch (error) {
       console.error(error);
-
-      if (error instanceof Error) {
-        if (
-          error.name == 'NotAllowedError' ||
-          error.name == 'PermissionDeniedError'
-        ) {
-          alert('Please allow this website to share your screen');
-        }
-      }
     }
   };
 
