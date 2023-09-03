b0a042b8c6319d28ecd53ad68c754930dfe1c13a
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-03 09:38:39 +0700

    feat: expose disable video and audio methods

--- app/_shared/sdk/room/stream/stream.ts
+++ app/_shared/sdk/room/stream/stream.ts
@@ -13,9 +13,50 @@ export const createStream = (peer: RoomPeerType.InstancePeer) => {
       this.origin = origin;
       this.source = source;
       this.mediaStream = mediaStream;
-      this.audioEnabled = true;
-      this.videoEnabled = true;
+      this.audioEnabled = mediaStream.getAudioTracks()[0]?.enabled || false;
+      this.videoEnabled = mediaStream.getVideoTracks()[0]?.enabled || false;
       this._peer = peer;
+    }
+
+    disableVideo = () => {
+      const peerConnection = this._peer.getPeerConnection();
+      if (!peerConnection) {
+        throw new Error('Cannot proceed. The peer is currently disconnected');
+      }
+
+      if (this.origin !== 'local') {
+        throw new Error(
+          'Cannot proceed. This method only works on local origin stream'
+        );
+      }
+
+      this._stopTrack(peerConnection, 'video');
+    };
+
+    disableAudio = () => {
+      const peerConnection = this._peer.getPeerConnection();
+      if (!peerConnection) {
+        throw new Error('Cannot proceed. The peer is currently disconnected');
+      }
+
+      if (this.origin !== 'local') {
+        throw new Error(
+          'Cannot proceed. This method only works on local origin stream'
+        );
+      }
+
+      this._stopTrack(peerConnection, 'audio');
+    };
+
+    _stopTrack(peerConnection: RTCPeerConnection, kind: 'video' | 'audio') {
+      for (const sender of peerConnection.getSenders()) {
+        if (!sender.track) return;
+
+        if (sender.track.kind === kind) {
+          sender.track.enabled = false;
+          sender.track.stop();
+        }
+      }
     }
   };
 
@@ -30,6 +71,8 @@ export const createStream = (peer: RoomPeerType.InstancePeer) => {
         mediaStream: stream.mediaStream,
         audioEnabled: stream.audioEnabled,
         videoEnabled: stream.videoEnabled,
+        disableVideo: stream.disableVideo,
+        disableAudio: stream.disableAudio,
       };
     },
   };
