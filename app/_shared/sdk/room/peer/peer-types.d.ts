bd466bc0b4622cbc171131df81c5467080731996
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-03 21:03:50 +0700

    feat: add more typings for peer

--- app/_shared/sdk/room/peer/peer-types.d.ts
+++ app/_shared/sdk/room/peer/peer-types.d.ts
@@ -6,11 +6,15 @@ export type InstancePeer = {
   disconnect: () => void;
   getPeerConnection: () => RTCPeerConnection | null;
   addStream: (key: string, value: RoomStreamType.StreamParams) => void;
-  removeStream: (key: string) => boolean;
+  removeStream: (key: string) => RoomStreamType.InstanceStream | null;
   getAllStreams: () => RoomStreamType.InstanceStream[];
   getStream: (key: string) => RoomStreamType.InstanceStream | null;
   getTotalStreams: () => number;
   hasStream: (key: string) => boolean;
+  enableCamera: () => void;
+  enableMic: () => void;
+  disableCamera: () => void;
+  disableMic: () => void;
 };
 
 export type PeerDependencies = {
@@ -24,6 +28,10 @@ export type PeerDependencies = {
 export type PeerEvents = {
   STREAM_ADDED: 'streamAdded';
   STREAM_REMOVED: 'streamRemoved';
+  _ADD_LOCAL_MEDIA_STREAM: 'addLocalMediaStream';
+  _ADD_REMOTE_MEDIA_STREAM: 'addRemoteMediaStream';
+  _ADD_LOCAL_SCREEN_STREAM: 'addLocalScreenStream';
+  _ADD_REMOTE_SCREEN_STREAM: 'addRemoteScreenStream';
 };
 
 export as namespace RoomPeerType;
