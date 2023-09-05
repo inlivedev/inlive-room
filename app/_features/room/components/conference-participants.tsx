bb7bd235b5cf57d5f6d0a788343f20f53c79a852
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-05 13:17:25 +0700

    refactor: use updated layout from room layout

--- app/_features/room/components/conference-participants.tsx
+++ app/_features/room/components/conference-participants.tsx
@@ -1,22 +1,103 @@
+'use client';
+
+import { useMemo, useRef } from 'react';
 import ConferenceScreen from '@/_features/room/components/conference-screen';
+import type { InstanceStream } from '@/_shared/sdk/room/stream/stream-types';
 import { useParticipantContext } from '@/_features/room/contexts/participant-context';
-import styles from '@/_features/room/styles/conference-participants.module.css';
+import styles from '@/_features/room/styles/conference.module.css';
+
+const isMobile = () => {
+  if (typeof window !== 'undefined') {
+    return window.innerWidth <= 768;
+  }
+};
 
 export default function ConferenceParticipants() {
   const { streams } = useParticipantContext();
+  const screenCount = useRef<number>(0);
+
+  const { medias, screens } = useMemo(() => {
+    const medias: InstanceStream[] = [];
+    const screens: InstanceStream[] = [];
+
+    for (const stream of streams) {
+      if (stream.source === 'screen') {
+        screens.push(stream);
+      } else {
+        medias.push(stream);
+      }
+    }
+
+    return { medias, screens };
+  }, [streams]);
+
+  const hasScreen = useMemo(() => {
+    return screens.length > 0;
+  }, [screens]);
+
+  const layout = (streams: InstanceStream[]) => {
+    if (hasScreen) {
+      return isMobile()
+        ? {
+            gridTemplateColumns: `repeat(${streams.length - 1}, 1fr)`,
+          }
+        : {
+            gridTemplateRows: `repeat(${streams.length - 1}, 1fr)`,
+          };
+    }
+  };
+
+  const videoLayout = (hasScreen: boolean, source: string) => {
+    if (hasScreen) {
+      if (source === 'screen') {
+        screenCount.current++;
+
+        if (screenCount.current === 1) {
+          return isMobile()
+            ? {
+                gridColumn: `1/span ${streams.length - 1}`,
+              }
+            : {
+                gridRow: `1/span ${streams.length - 1}`,
+              };
+        }
+        return {
+          gridRow: `auto/auto`,
+          gridColumn: `2/auto`,
+          minHeight: 0,
+          height: '100%',
+        };
+      }
+    }
+  };
+
+  const renderVideo = (stream: InstanceStream) => {
+    return (
+      <div
+        key={stream.id}
+        className={`${
+          hasScreen
+            ? stream.source === 'screen'
+              ? styles['presenting-screen']
+              : styles['presenting-media']
+            : styles['gallery-media']
+        }`}
+        style={videoLayout(hasScreen, stream.source)}
+      >
+        <ConferenceScreen stream={stream} />
+      </div>
+    );
+  };
 
   return (
-    <div className={`${styles['participants-grid']}`}>
-      {streams.map((stream) => {
-        return (
-          <div
-            key={stream.mediaStream.id}
-            className={`${styles['participants-grid-screen']}`}
-          >
-            <ConferenceScreen stream={stream} />
-          </div>
-        );
-      })}
+    <div
+      className={`${
+        styles[hasScreen ? 'presenting' : 'gallery']
+      } row-start-1 w-full p-4 ${styles['media']}`}
+      style={layout(screens)}
+    >
+      {screens ? screens.map(renderVideo) : null}
+      {medias ? medias.map(renderVideo) : null}
     </div>
   );
 }
--- app/_features/room/components/conference-screen.tsx
+++ app/_features/room/components/conference-screen.tsx
@@ -11,18 +11,9 @@ export default function ConferenceScreen({
   const { videoRef } = useVideoScreen(stream);
 
   return (
-    <div
-      className="relative h-full w-full overflow-hidden rounded-md"
-      style={{
-        paddingBottom: 'calc(2 / 3 * 100%)',
-      }}
-    >
-      <div className="absolute left-0 top-0 h-full w-full">
-        <video
-          className="absolute left-0 top-0 h-full w-full object-cover object-center"
-          ref={videoRef}
-        ></video>
-      </div>
-    </div>
+    <video
+      className="rounded-lg object-contain object-center "
+      ref={videoRef}
+    ></video>
   );
 }
