2a63307549b94d0a38586af2f65552f203d78b87
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-15 18:38:50 +0700

    fix: base url and version should be from .env

--- app/_shared/utils/sdk.ts
+++ app/_shared/utils/sdk.ts
@@ -1,3 +1,11 @@
 import { Room } from '@/_shared/sdk/room';
 
-export const room = Room();
+const hubOrigin = process.env.NEXT_PUBLIC_HUB_ORIGIN;
+const apiVersion = process.env.NEXT_PUBLIC_API_VERSION;
+
+export const room = Room({
+  api: {
+    baseUrl: hubOrigin,
+    version: apiVersion,
+  },
+});
