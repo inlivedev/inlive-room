1e6688db59106b3d83c612bb43c2df7b21a2ea13
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-18 13:57:19 +0700

    refactor: change file name import

--- app/_features/room/components/conference-actions-bar.tsx
+++ app/_features/room/components/conference-actions-bar.tsx
@@ -1,16 +1,16 @@
 import ButtonLeave from '@/_features/room/components/button-leave';
-import ButtonToggleMicrophone from '@/_features/room/components/button-toggle-microphone';
-import ButtonToggleCamera from '@/_features/room/components/button-toggle-camera';
+import ButtonMicrophone from '@/_features/room/components/button-microphone';
+import ButtonCamera from '@/_features/room/components/button-camera';
 import ButtonScreenShare from '@/_features/room/components/button-screen-share';
 
 export default function ConferenceActionsBar() {
   return (
     <div className="flex h-full w-full items-center justify-center gap-4 border-t border-neutral-700 px-4 py-1.5 md:py-2.5 lg:gap-6">
       <div className="flex h-full flex-col justify-center">
-        <ButtonToggleMicrophone />
+        <ButtonMicrophone />
       </div>
       <div className="flex h-full flex-col justify-center">
-        <ButtonToggleCamera />
+        <ButtonCamera />
       </div>
       <div className="flex h-full flex-col justify-center">
         <ButtonScreenShare />
