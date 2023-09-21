9527606b2bd45fb4633c9087549ecb93b8781cb7
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-21 13:59:59 +0700

    feat: add 401 and 403 validation and responses

--- app/(server)/api/auth/current/route.ts
+++ app/(server)/api/auth/current/route.ts
@@ -14,14 +14,34 @@ export async function GET() {
 
   if (!token) {
     return NextResponse.json(
-      { code: 200, message: 'OK', ok: true, data: null },
       {
-        status: 200,
+        code: 401,
+        message:
+          'Unauthorized. Credential is needed to continue proceed with the request.',
+        ok: false,
+        data: null,
+      },
+      {
+        status: 401,
       }
     );
   }
 
   const response = await getCurrentAuthenticated(token);
+
+  if (response.code === 403) {
+    return NextResponse.json(
+      {
+        code: response.code,
+        message: 'Forbidden. Credential is invalid',
+        ok: false,
+        data: null,
+      },
+      {
+        status: response.code,
+      }
+    );
+  }
 
   return NextResponse.json(response, {
     status: response.code,
