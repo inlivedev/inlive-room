6d4f8ab0ccd138500f4b53ac9add1bf4073ef9b3
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-21 10:14:32 +0700

    refactor: remove unused util

--- app/_shared/utils/api.ts
+++
@@ -1,5 +0,0 @@
-import { createFetcher } from "@/(server)/_shared/fetcher/fetcher";
-
-const InternalAPI = process.env.BASE_API
-
-export const InternalAPIFetcher = createFetcher().createInstance(InternalAPI|| "localhost:3000")
