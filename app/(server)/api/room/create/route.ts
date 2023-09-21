7e1452cdc151815198803de9db06a8b48a4d1ae9
gagahgk <gagah.khaesarrahman@asumsi.co>
2023-09-21 16:04:17 +0700

    fix: create endpoint not using app router

--- app/(server)/api/room/create/route.ts
+++ app/(server)/api/room/create/route.ts
@@ -1,30 +1,43 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { apiResponse } from '../../../_shared/types';
 import { roomRoutesHandler } from '../../../_features/room/routes';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const requestToken = req.cookies['token'];
+export async function POST() {
+  const cookieStore = cookies();
+  const requestToken = cookieStore.get('token');
 
   if (!requestToken) {
-    res.status(401).json({
-      code: 401,
-      message: 'Please check if token is provided in the cookie',
-    } as apiResponse);
+    NextResponse.json(
+      {
+        code: 401,
+        message: 'Please check if token is provided in the cookie',
+      } as apiResponse,
+      { status: 401 }
+    );
     return;
   }
 
   try {
-    const createdRoom = await roomRoutesHandler.createRoomHandler(requestToken);
+    const createdRoom = await roomRoutesHandler.createRoomHandler(
+      requestToken.value
+    );
 
-    res.status(201).json({
-      code: 201,
-      message: 'Room Created',
-      data: createdRoom,
-    } as apiResponse);
+    NextResponse.json(
+      {
+        code: 201,
+        message: 'Room Created',
+        data: createdRoom,
+      } as apiResponse,
+      { status: 201 }
+    );
   } catch (e) {
-    res.status(500).json({
-      code: 500,
-      message: 'An error has occured on our side, please try again later',
-    } as apiResponse);
+    NextResponse.json(
+      {
+        code: 500,
+        message: 'An error has occured on our side, please try again later',
+      } as apiResponse,
+      { status: 500 }
+    );
   }
 }
