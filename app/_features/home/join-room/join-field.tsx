d5d070026a13f5e5da2e4c4dea621fcc348bc650
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-22 19:35:49 +0700

    style: update color to zinc

--- app/_features/home/join-room/join-field.tsx
+++ app/_features/home/join-room/join-field.tsx
@@ -8,7 +8,7 @@ export default function JoinField() {
   return (
     <div className="mt-8 flex flex-1 items-center justify-between gap-4">
       <input
-        className="flex-1 rounded-md border border-neutral-600 bg-neutral-800 px-4 py-2 text-sm outline-none placeholder:text-neutral-500 focus:border-neutral-500"
+        className="flex-1 rounded-md border border-zinc-600 bg-zinc-800 px-4 py-2 text-sm outline-none placeholder:text-zinc-500 focus:border-zinc-500"
         type="text"
         placeholder="Enter a room code"
         {...bindField}
