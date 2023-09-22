0641642e42f3ed4abab3f4ece6e2298bb0f8b7ed
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-22 19:36:14 +0700

    feat: auth types under AuthType namespace

---
+++ app/_shared/types/auth.d.ts
@@ -0,0 +1,42 @@
+import type { FetcherResponse } from '@/_shared/utils/fetcher';
+
+export declare namespace AuthType {
+  type AuthorizeResponse = FetcherResponse & {
+    message: string;
+    data: string;
+  };
+
+  type AuthenticateExternalResponse = FetcherResponse & {
+    message: string;
+    data: {
+      token: string;
+    };
+  };
+
+  type AuthenticateInternalResponse = FetcherResponse & {
+    message: string;
+    data: {
+      token: string;
+    } | null;
+  };
+
+  type UserData = {
+    email: string;
+    id: number;
+    login_type: number;
+    name: string;
+    picture_url: string;
+    role_id: number;
+    username: string;
+  };
+
+  type CurrentAuthExternalResponse = FetcherResponse & {
+    message: string;
+    data: UserData;
+  };
+
+  type CurrentAuthInternalResponse = FetcherResponse & {
+    message: string;
+    data: UserData | null;
+  };
+}
