96a6a8acdebc646494b8bd72809f28cbf572f1ba
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-22 19:40:28 +0700

    feat: wrap the home layout inside the authprovider

--- app/_features/home/layout/layout.tsx
+++ app/_features/home/layout/layout.tsx
@@ -1,25 +1,35 @@
-import Header from '@/_shared/components/header/header';
-import CreateRoom from '@/_features/home/create-room';
-import JoinRoom from '@/_features/home/join-room';
-import Footer from '@/_shared/components/footer/footer';
+'use client';
 
-export default function Layout() {
+import AuthProvider from '@/_shared/providers/auth';
+import Header from '@/_shared/components/header/header';
+import CreateRoom from '@/_features/home/create-room/create-room';
+import JoinRoom from '@/_features/home/join-room/join-room';
+import Footer from '@/_shared/components/footer/footer';
+import type { AuthType } from '@/_shared/types/auth';
+
+export default function View({
+  currentUser,
+}: {
+  currentUser: AuthType.UserData | null;
+}) {
   return (
-    <div className="flex flex-1 flex-col bg-neutral-900 text-neutral-200">
-      <div className="mx-auto flex w-full max-w-5xl flex-1 flex-col gap-10 px-4">
-        <Header />
-        <main className="mx-auto flex flex-1 flex-col justify-center">
-          <div className="flex w-full flex-col gap-20 lg:flex-row">
-            <div>
-              <CreateRoom />
+    <AuthProvider value={{ currentUser: currentUser }}>
+      <div className="flex flex-1 flex-col bg-zinc-900 text-zinc-200">
+        <div className="mx-auto flex w-full max-w-5xl flex-1 flex-col gap-10 px-4">
+          <Header />
+          <main className="mx-auto flex flex-1 flex-col justify-center">
+            <div className="flex w-full flex-col gap-20 lg:flex-row">
+              <div>
+                <CreateRoom />
+              </div>
+              <div className="flex-1">
+                <JoinRoom />
+              </div>
             </div>
-            <div className="flex-1">
-              <JoinRoom />
-            </div>
-          </div>
-        </main>
-        <Footer />
+          </main>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </AuthProvider>
   );
 }
