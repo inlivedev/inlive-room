31cc780886f4486f1fa78aa662f203ec9dc610a9
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-25 10:54:14 +0700

    style: wrap input with flex div

--- app/_features/home/join-room/join-field.tsx
+++ app/_features/home/join-room/join-field.tsx
@@ -7,12 +7,14 @@ export default function JoinField() {
 
   return (
     <div className="mt-8 flex flex-1 items-center justify-between gap-4">
-      <input
-        className="flex-1 rounded-md border border-zinc-600 bg-zinc-800 px-4 py-2 text-sm outline-none placeholder:text-zinc-500 focus:border-zinc-500"
-        type="text"
-        placeholder="Enter a room code"
-        {...bindField}
-      />
+      <div className="flex-1">
+        <input
+          className="w-full rounded-md border border-zinc-600 bg-zinc-800 px-4 py-2 text-sm outline-none placeholder:text-zinc-500 focus:border-zinc-500"
+          type="text"
+          placeholder="Enter a room code"
+          {...bindField}
+        />
+      </div>
       <button
         className="text-sm font-medium"
         onClick={(event) => joinRoom(event)}
