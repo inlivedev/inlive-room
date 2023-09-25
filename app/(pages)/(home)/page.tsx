adee89e1b35b7abbb808bfb0527e144df70ee804
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-25 10:20:27 +0700

    feat: pass current user data from page server component

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,11 +1,21 @@
 import type { Metadata } from 'next';
+import { headers } from 'next/headers';
 import Home from '@/_features/home/layout';
+import { InternalApiFetcher } from '@/_shared/utils/fetcher';
+import type { AuthType } from '@/_shared/types/auth';
 
 export const metadata: Metadata = {
   title: 'inLive Room',
   description: 'Conference room for real-time video and audio calls',
 };
 
-export default function Page() {
-  return <Home />;
+export default async function Page() {
+  const currentAuth: AuthType.CurrentAuthInternalResponse =
+    await InternalApiFetcher.get('/api/auth/current', {
+      headers: headers(),
+    });
+
+  const currentUser = currentAuth.data ? currentAuth.data : null;
+
+  return <Home currentUser={currentUser} />;
 }
