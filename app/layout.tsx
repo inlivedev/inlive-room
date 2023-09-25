3608c5abe5948cd92551439962ef9506fcfaaed4
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-25 11:06:09 +0700

    fix: duplicate charset and viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* charset and viewport are added automatically */}
         <link rel="icon" href="/images/favicon/favicon.ico" sizes="any" />
         <link
           rel="icon"
