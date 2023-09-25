be4e48b3551e0f7087fa093dc9728b0e775fe199
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-25 12:32:54 +0700

    feat: dispatch open:sign-in-modal event on sign in button

--- app/_shared/components/header/header.tsx
+++ app/_shared/components/header/header.tsx
@@ -1,25 +1,45 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { Button } from '@nextui-org/react';
 
 export default function Header() {
+  const openSignInModal = () => {
+    document.dispatchEvent(new CustomEvent('open:sign-in-modal'));
+  };
+
   return (
     <header className="w-full py-6 lg:py-8">
-      <Link href="/" className="flex items-center justify-center gap-2">
-        <Image
-          src="/images/favicon/favicon.svg"
-          alt="Icon Logo"
-          className="h-6 w-6 lg:h-7 lg:w-7"
-          width={28}
-          height={28}
-        />
-        <Title text="inLive Room" />
-      </Link>
+      <div className="flex items-center justify-between">
+        <div className="flex flex-1 items-center">
+          <Link
+            href="/"
+            className="inline-flex items-center justify-center gap-2"
+          >
+            <Image
+              src="/images/favicon/favicon.svg"
+              alt="Icon Logo"
+              className="h-6 w-6 lg:h-7 lg:w-7"
+              width={28}
+              height={28}
+            />
+            <h1 className="text-lg font-semibold tracking-wide lg:text-xl">
+              inLive Room
+            </h1>
+          </Link>
+        </div>
+        <div>
+          <Button
+            variant="flat"
+            size="sm"
+            className="bg-zinc-800 hover:bg-zinc-700 active:bg-zinc-600 sm:h-unit-9 sm:w-unit-20 sm:px-4 sm:text-sm"
+            onClick={openSignInModal}
+          >
+            Sign In
+          </Button>
+        </div>
+      </div>
     </header>
-  );
-}
-
-function Title({ text }: { text: string }) {
-  return (
-    <h1 className="text-lg font-semibold tracking-wide lg:text-xl">{text}</h1>
   );
 }
