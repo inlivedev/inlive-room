901c3508104a6a01fb923a10fbcf563d45afd2f0
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-26 09:46:27 +0700

    feat: wrap app container for not found layout

--- app/(pages)/room/[roomId]/not-found.tsx
+++ app/(pages)/room/[roomId]/not-found.tsx
@@ -1,11 +1,18 @@
+import AppContainer from '@/_shared/components/containers/app-container';
 import HTTPError from '@/_shared/components/errors/http-error';
+import { getClientAuth } from '@/_shared/utils/get-client-auth';
 
-export default function NotFound() {
+export default async function NotFound() {
+  const currentAuth = await getClientAuth();
+  const currentUser = currentAuth.data ? currentAuth.data : undefined;
+
   return (
-    <HTTPError
-      code={404}
-      title="Room Not Found"
-      description="There is nothing to see on this page"
-    />
+    <AppContainer currentUser={currentUser}>
+      <HTTPError
+        code={404}
+        title="Room Not Found"
+        description="There is nothing to see on this page"
+      />
+    </AppContainer>
   );
 }
