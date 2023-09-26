bd8342cd645bb55d54ee48b969d0b92b8d26d0d2
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-26 09:38:04 +0700

    refactor: container only consists of room specific provider

--- app/_features/room/components/container.tsx
+++ app/_features/room/components/container.tsx
@@ -1,26 +1,19 @@
 'use client';
 
-import View from '@/_features/room/components/view';
 import { PeerProvider } from '@/_features/room/contexts/peer-context';
-import type { AuthType } from '@/_shared/types/auth';
-import AuthProvider from '@/_shared/providers/auth';
 
 export default function Container({
   roomId,
   clientId,
-  origin,
-  currentUser,
+  children,
 }: {
   roomId: string;
   clientId: string;
-  origin: string;
-  currentUser: AuthType.UserData | undefined;
+  children: React.ReactNode;
 }) {
   return (
-    <AuthProvider value={{ currentUser: currentUser }}>
-      <PeerProvider roomId={roomId} clientId={clientId}>
-        <View roomId={roomId} origin={origin} />
-      </PeerProvider>
-    </AuthProvider>
+    <PeerProvider roomId={roomId} clientId={clientId}>
+      {children}
+    </PeerProvider>
   );
 }
