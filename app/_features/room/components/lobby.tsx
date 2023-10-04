685739ad61a17c58cc1ac5d8ec5263679108edcf
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-10-04 20:47:25 +0700

    feat: import lobby registration and lobby entrance views

--- app/_features/room/components/lobby.tsx
+++ app/_features/room/components/lobby.tsx
@@ -1,17 +1,22 @@
 import Header from '@/_shared/components/header/header';
 import Footer from '@/_shared/components/footer/footer';
+import LobbyRegistration from '@/_features/room/components/lobby-registration';
+import LobbyEntrance from '@/_features/room/components/lobby-entrance';
 
-type LobbyProps = {
-  children: React.ReactNode;
-};
-
-export default function Lobby({ children }: LobbyProps) {
+export default function Lobby({
+  pageId,
+  origin,
+}: {
+  pageId: string;
+  origin: string;
+}) {
   return (
     <div className="flex min-h-screen flex-col">
       <div className="mx-auto flex w-full max-w-xl flex-1 flex-col gap-10 px-4">
         <Header />
-        <main className="flex flex-1 flex-col justify-center">
-          <div className="flex w-full max-w-xl flex-col">{children}</div>
+        <main className="flex flex-1 flex-col">
+          <LobbyRegistration pageId={pageId} />
+          <LobbyEntrance pageId={pageId} origin={origin} />
         </main>
         <Footer />
       </div>
