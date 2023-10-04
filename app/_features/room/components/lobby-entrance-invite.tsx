daea30fb7c12628161bc88ec53206cec642a0028
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-10-04 20:06:56 +0700

    refactor: rename to lobby entrance invite

--- app/_features/room/components/lobby-entrance-invite.tsx
+++ app/_features/room/components/lobby-entrance-invite.tsx
@@ -1,4 +1,5 @@
 import { copyToClipboard } from '@/_shared/utils/copy-to-clipboard';
+import { Button } from '@nextui-org/react';
 
 export default function LobbyInvite({
   pageId,
@@ -17,29 +18,29 @@ export default function LobbyInvite({
   };
 
   return (
-    <div className="mt-10 flex flex-col gap-6 rounded-lg px-4 py-5 ring-1 ring-neutral-700 lg:p-6">
+    <div className="flex flex-col gap-4 rounded-lg px-4 py-5 ring-1 ring-zinc-700 md:p-6">
       <div>
         <h2 className="font-medium">Share this room</h2>
-        <p className="mt-0.5 text-sm text-neutral-400">
+        <p className="mt-0.5 text-sm text-zinc-400">
           Invite others to join by sending this link
         </p>
       </div>
       <div className="flex gap-4">
         <div className="flex-1">
           <input
-            className="w-full rounded-md bg-neutral-900 px-4 py-2 text-sm text-neutral-200 outline-none ring-1 ring-neutral-700  focus-visible:ring-1 focus-visible:ring-neutral-200"
+            className="w-full rounded-md bg-zinc-800 px-4 py-2.5 text-sm text-zinc-200 outline-none ring-1 ring-zinc-700 focus-visible:ring-1 focus-visible:ring-zinc-400"
             type="text"
             readOnly
             value={`${origin}/room/${pageId}`}
           />
         </div>
         <div>
-          <button
-            className="rounded-md bg-neutral-800 px-4 py-2 text-sm font-medium hover:bg-neutral-800/60"
+          <Button
+            className="rounded-md bg-zinc-800 px-4 py-2 text-sm hover:bg-zinc-700 active:bg-zinc-600"
             onClick={() => handleCopyLink(`${origin}/room/${pageId}`)}
           >
             Copy Link
-          </button>
+          </Button>
         </div>
       </div>
     </div>
