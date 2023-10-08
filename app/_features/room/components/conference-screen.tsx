b7a45b6dc61f924be67b04814ad1abbd190f2f72
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-10-08 15:57:35 +0700

    refactor: update comment

--- app/_features/room/components/conference-screen.tsx
+++ app/_features/room/components/conference-screen.tsx
@@ -12,7 +12,7 @@ export default function ConferenceScreen({
 
   return (
     <div className="video-screen relative rounded-lg shadow-lg">
-      {/* overlay video screen */}
+      {/* video screen overlay */}
       <div className="absolute flex h-full w-full flex-col justify-end rounded-lg p-2">
         <div className="flex">
           <div className="video-screen-name max-w-full truncate rounded bg-zinc-900/70 px-2 py-0.5 text-xs font-medium text-zinc-200 md:text-sm">
