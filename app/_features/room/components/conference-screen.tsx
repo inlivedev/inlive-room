402527dc69a2d58093572a3bf8551e75dd3ff231
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-10-08 16:49:13 +0700

    style: import css module styles and adjust video element to be the same as container

--- app/_features/room/components/conference-screen.tsx
+++ app/_features/room/components/conference-screen.tsx
@@ -2,6 +2,7 @@
 
 import { useVideoScreen } from '@/_features/room/hooks/use-video-screen';
 import type { ParticipantStream } from '@/_features/room/contexts/participant-context';
+import styles from '@/_features/room/styles/conference.module.css';
 
 export default function ConferenceScreen({
   stream,
@@ -11,17 +12,19 @@ export default function ConferenceScreen({
   const { videoRef } = useVideoScreen(stream);
 
   return (
-    <div className="video-screen relative rounded-lg shadow-lg">
+    <div className={`${styles['video-screen']} relative rounded-lg shadow-lg`}>
       {/* video screen overlay */}
       <div className="absolute flex h-full w-full flex-col justify-end rounded-lg p-2">
         <div className="flex">
-          <div className="video-screen-name max-w-full truncate rounded bg-zinc-900/70 px-2 py-0.5 text-xs font-medium text-zinc-200 md:text-sm">
+          <div
+            className={`${styles['video-screen-name']} max-w-full truncate rounded bg-zinc-900/70 px-2 py-0.5 text-xs font-medium text-zinc-200 md:text-sm`}
+          >
             <span>{stream.name}</span>
           </div>
         </div>
       </div>
       <video
-        className="rounded-lg object-contain object-center"
+        className="aspect-video h-full rounded-lg object-cover object-center"
         ref={videoRef}
       ></video>
     </div>
