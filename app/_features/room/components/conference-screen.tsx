09b34d0c5fb06d57d551af1ea98bc70f4236ef44
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-10-08 15:56:35 +0700

    feat: display participant name on overlay video screen

--- app/_features/room/components/conference-screen.tsx
+++ app/_features/room/components/conference-screen.tsx
@@ -11,9 +11,19 @@ export default function ConferenceScreen({
   const { videoRef } = useVideoScreen(stream);
 
   return (
-    <video
-      className="rounded-lg object-contain object-center "
-      ref={videoRef}
-    ></video>
+    <div className="video-screen relative rounded-lg shadow-lg">
+      {/* overlay video screen */}
+      <div className="absolute flex h-full w-full flex-col justify-end rounded-lg p-2">
+        <div className="flex">
+          <div className="video-screen-name max-w-full truncate rounded bg-zinc-900/70 px-2 py-0.5 text-xs font-medium text-zinc-200 md:text-sm">
+            <span>{stream.name}</span>
+          </div>
+        </div>
+      </div>
+      <video
+        className="rounded-lg object-contain object-center"
+        ref={videoRef}
+      ></video>
+    </div>
   );
 }
