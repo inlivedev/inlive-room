cb5174ea7f08684beecd2caef2aa3fcbe2de20c4
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-10-08 08:16:13 +0700

    fix: prevent images url on middleware

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,5 @@ import { withRoomMiddleware } from './middlewares/room';
 export default chain([withRoomMiddleware, withAuthMiddleware]);
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|images).*)'],
 };
