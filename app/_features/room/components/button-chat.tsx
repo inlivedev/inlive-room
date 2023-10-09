2f0f11fe287f94777c31cb266b399c683adff9f5
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-10-10 05:44:19 +0700

    refactor: simplify button chat component

--- app/_features/room/components/button-chat.tsx
+++ app/_features/room/components/button-chat.tsx
@@ -1,64 +1,22 @@
-import { useEffect, useState } from 'react';
-import { Badge, Button, Spinner } from '@nextui-org/react';
-import { usePeerContext } from '../contexts/peer-context';
+'use client';
 
-export default function ButtonChat({ onOpen }: { onOpen: () => void }) {
-  const { peer } = usePeerContext();
-  const [numChat, setNumChat] = useState(0);
-  const [isChannelOpen, setChannelOpen] = useState(false);
+import { Button } from '@nextui-org/react';
+import ChatIcon from '@/_shared/components/icons/chat-icon';
 
-  useEffect(() => {
-    const peerConnection = peer?.getPeerConnection();
-
-    if (!peerConnection) return;
-
-    peerConnection.addEventListener('datachannel', (chEevent) => {
-      const receiveChannel = chEevent.channel;
-      receiveChannel.binaryType = 'arraybuffer';
-
-      receiveChannel.addEventListener('message', () => {
-        if (receiveChannel.label == 'chat') {
-          console.log(numChat);
-          setNumChat((prevNumChat) => prevNumChat + 1);
-          console.log(numChat);
-        }
-      });
-
-      receiveChannel.addEventListener('open', () => {
-        if (receiveChannel.label == 'chat') {
-          setChannelOpen(true);
-        }
-      });
-
-      receiveChannel.addEventListener('close', () => {
-        if (receiveChannel.label == 'chat') {
-          setChannelOpen(false);
-        }
-      });
-    });
-  }, [peer, numChat]);
-
-  const resetBadge = () => {
-    onOpen();
-    setNumChat(0);
+export default function ButtonChat() {
+  const openChat = () => {
+    document.dispatchEvent(new CustomEvent('open:room-chat-menu'));
   };
 
   return (
-    <Badge
-      content={isChannelOpen ? numChat : <Spinner size="sm"></Spinner>}
-      isInvisible={isChannelOpen && numChat == 0 ? true : false}
+    <Button
+      isIconOnly
+      variant="flat"
+      aria-label="Toggle chat menu"
+      className="bg-zinc-700/70 hover:bg-zinc-600 active:bg-zinc-500"
+      onClick={openChat}
     >
-      <Button
-        isDisabled={false}
-        isIconOnly
-        variant="flat"
-        onClick={resetBadge}
-        className={`
-        bg-zinc-700/70 
-        hover:bg-zinc-600 
-        active:bg-zinc-500
-        `}
-      ></Button>
-    </Badge>
+      <ChatIcon width={20} height={20} />
+    </Button>
   );
 }
