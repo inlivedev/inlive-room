8f14d2ff5a3ad3246ad281b71562c115ea447618
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-10-16 13:15:07 +0700

    fix: handle setViewportHeight on resize with debounce (#69)

--- app/_shared/hooks/use-viewport-height.ts
+++ app/_shared/hooks/use-viewport-height.ts
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 
+let timeout: ReturnType<typeof setTimeout> | null = null;
+
 const useViewportHeight = () => {
   useEffect(() => {
     const setViewportHeight = () => {
@@ -7,11 +9,21 @@ const useViewportHeight = () => {
       document.documentElement.style.setProperty('--vh', `${vh}px`);
     };
 
+    const onOrientationChange = () => setViewportHeight();
+
+    const onResize = () => {
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(setViewportHeight, 250);
+    };
+
+    window.addEventListener('orientationchange', onOrientationChange);
+    window.addEventListener('resize', onResize);
+
     setViewportHeight();
-    window.addEventListener('orientationchange', setViewportHeight);
 
     return () => {
-      window.removeEventListener('orientationchange', setViewportHeight);
+      window.removeEventListener('orientationchange', onOrientationChange);
+      window.removeEventListener('resize', onResize);
     };
   }, []);
 };
