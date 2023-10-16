8c04f540c33c957c0e6f6b8f9f5a56791d324826
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-10-16 08:43:32 +0700

    fix: disabling leave room when waiting for submit

--- app/_features/room/components/button-leave.tsx
+++ app/_features/room/components/button-leave.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@nextui-org/react';
+import { Button, Spinner } from '@nextui-org/react';
 import HangUpIcon from '@/_shared/components/icons/hang-up-icon';
 import { useLeaveRoom } from '@/_features/room/hooks/use-leave-room';
 
 export default function ButtonLeave() {
-  const { leaveRoom } = useLeaveRoom();
+  const { leaveRoom, isSubmitting } = useLeaveRoom();
 
   return (
     <Button
@@ -12,8 +12,21 @@ export default function ButtonLeave() {
       aria-label="Leave from this room"
       className="bg-red-600/70 hover:bg-red-600 focus:outline-zinc-100 active:bg-red-500"
       onClick={leaveRoom}
+      isDisabled={isSubmitting}
+      aria-disabled={isSubmitting}
+      disabled={isSubmitting}
     >
-      <HangUpIcon width={20} height={20} />
+      {isSubmitting ? (
+        <Spinner
+          classNames={{
+            circle1: 'border-b-zinc-200',
+            circle2: 'border-b-zinc-200',
+            wrapper: 'w-4 h-4',
+          }}
+        />
+      ) : (
+        <HangUpIcon width={20} height={20} />
+      )}
     </Button>
   );
 }
--- app/_features/room/hooks/use-leave-room.ts
+++ app/_features/room/hooks/use-leave-room.ts
@@ -1,28 +1,41 @@
+import { useState, useCallback } from 'react';
 import { room } from '@/_shared/utils/sdk';
 import { usePeerContext } from '@/_features/room/contexts/peer-context';
 import { useClientContext } from '@/_features/room/contexts/client-context';
 
 export const useLeaveRoom = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   const { peer } = usePeerContext();
   const { clientID, roomID } = useClientContext();
 
-  const leaveRoom = async () => {
+  const leaveRoom = useCallback(async () => {
     if (!peer) return;
 
-    peer.disconnect();
-    room
-      .leaveRoom(roomID, clientID)
-      .then((response) => {
-        if (response.code >= 300) {
-          console.error('Failed to end the call');
-        }
-      })
-      .catch((error) => {
+    if (!isSubmitting) {
+      setIsSubmitting(true);
+
+      try {
+        peer.disconnect();
+        room
+          .leaveRoom(roomID, clientID)
+          .then((response) => {
+            if (response.code >= 300) {
+              console.error('Failed to end the call');
+            }
+          })
+          .catch((error) => {
+            throw error;
+          });
+
+        setIsSubmitting(false);
+        window.location.href = '/';
+      } catch (error) {
+        setIsSubmitting(false);
         console.error(error);
-      });
+      }
+    }
+  }, [roomID, clientID, peer, isSubmitting]);
 
-    window.location.href = '/';
-  };
-
-  return { leaveRoom };
+  return { leaveRoom, isSubmitting };
 };
