af1be4f3d1454d43a9a90a5611e6cb37501addb6
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-10-17 07:06:31 +0700

    feat: check if local stream has existed

--- app/_features/room/components/event-container.tsx
+++ app/_features/room/components/event-container.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useEffect } from 'react';
 import { usePeerContext } from '@/_features/room/contexts/peer-context';
+import { useParticipantContext } from '@/_features/room/contexts/participant-context';
 import { hasTouchScreen } from '@/_shared/utils/has-touch-screen';
 
 export default function EventContainer({
@@ -9,24 +10,31 @@ export default function EventContainer({
   children: React.ReactNode;
 }) {
   const { peer } = usePeerContext();
+  const { streams } = useParticipantContext();
 
   useEffect(() => {
     if (!peer) return;
 
+    const localStream = streams.find(
+      (stream) => stream.origin === 'local' && stream.source === 'media'
+    );
+
+    if (!localStream) return;
+
     const onTurnOnCamera = () => {
-      if (peer) peer.turnOnCamera();
+      if (peer && localStream) peer.turnOnCamera();
     };
 
     const onTurnOffCamera = () => {
-      if (peer) peer.turnOffCamera();
+      if (peer && localStream) peer.turnOffCamera();
     };
 
     const onTurnOnMic = () => {
-      if (peer) peer.turnOnMic();
+      if (peer && localStream) peer.turnOnMic();
     };
 
     const onTurnOffMic = () => {
-      if (peer) peer.turnOffMic();
+      if (peer && localStream) peer.turnOffMic();
     };
 
     document.addEventListener('trigger:turnon-camera', onTurnOnCamera);
@@ -40,14 +48,13 @@ export default function EventContainer({
       document.removeEventListener('trigger:turnon-mic', onTurnOnMic);
       document.removeEventListener('trigger:turnoff-mic', onTurnOffMic);
     };
-  }, [peer]);
+  }, [peer, streams]);
 
   useEffect(() => {
     if (!peer) return;
+    const isTouchScreen = hasTouchScreen();
 
     const onVisibilityChange = () => {
-      const isTouchScreen = hasTouchScreen();
-
       if (document.visibilityState === 'hidden' && isTouchScreen && peer) {
         document.dispatchEvent(new CustomEvent('trigger:turnoff-camera'));
         document.dispatchEvent(new CustomEvent('trigger:turnoff-mic'));
