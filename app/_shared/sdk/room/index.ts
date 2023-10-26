2e433a3101abd82f9be644da82a6ded810edf72e
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-10-26 14:04:46 +0700

    refactor: remove sdk-related files

--- app/_shared/sdk/room/index.ts
+++
@@ -1,9 +0,0 @@
-import { facade } from './facade/facade';
-
-const createRoom = (facade: RoomFacadeType.Facade) => {
-  return (config: RoomType.UserConfig = {}) => {
-    return facade.createInstance(config);
-  };
-};
-
-export const Room = createRoom(facade);
