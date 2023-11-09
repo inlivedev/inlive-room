7be3fbf689d547dbde4d82a9bc337b878d8ddf20
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-11-09 14:56:16 +0700

    refactor: use RoomEvent

--- app/_shared/utils/sdk.ts
+++ app/_shared/utils/sdk.ts
@@ -1,4 +1,5 @@
 import { Room } from '@inlivedev/inlive-js-sdk/dist/room';
+export { RoomEvent } from '@inlivedev/inlive-js-sdk/dist/room';
 
 const inliveHubOrigin = process.env.NEXT_PUBLIC_INLIVE_HUB_ORIGIN;
 const inliveHubVersion = process.env.NEXT_PUBLIC_INLIVE_HUB_VERSION;
