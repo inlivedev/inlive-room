e09df8a36785ef59a453e63f0b8f8d9690eca533
gagahgk <gagah.khaesarrahman@asumsi.co>
2023-12-05 10:24:38 +0700

    feat: add inlive api key to cloudbuild

--- app/(server)/_shared/utils/sdk.ts
+++ app/(server)/_shared/utils/sdk.ts
@@ -2,7 +2,7 @@ import { Room } from '@inlivedev/inlive-js-sdk/dist/room';
 
 const inliveHubOrigin = process.env.NEXT_PUBLIC_INLIVE_HUB_ORIGIN;
 const inliveHubVersion = process.env.NEXT_PUBLIC_INLIVE_HUB_VERSION;
-const inliveHubApiKey = process.env.NEXT_PUBLIC_INLIVE_HUB_API_KEY;
+const inliveHubApiKey = process.env.INLIVE_HUB_API_KEY;
 
 export const serverSDK = Room({
   api: {
