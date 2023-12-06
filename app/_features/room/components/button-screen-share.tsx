3cd7b8142ce9d04ed3f271988103c9f2f8c3837a
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-12-07 01:20:43 +0700

    fix: fix button muted style and restrict only for speaker (#111)

--- app/_features/room/components/button-screen-share.tsx
+++ app/_features/room/components/button-screen-share.tsx
@@ -1,18 +1,31 @@
 'use client';
 
+import { useMemo } from 'react';
 import { Button } from '@nextui-org/react';
 import ScreenShareOnIcon from '@/_shared/components/icons/screen-share-on-icon';
 import ScreenShareOffIcon from '@/_shared/components/icons/screen-share-off-icon';
 import { useScreenShare } from '@/_features/room/hooks/use-screen-share';
+import { useClientContext } from '@/_features/room/contexts/client-context';
+import { useMetadataContext } from '@/_features/room/contexts/metadata-context';
 
 export default function ButtonScreenShare() {
   const { startScreenCapture, stopScreenCapture, screenCaptureActive } =
     useScreenShare();
 
+  const { clientID } = useClientContext();
+  const { host, speakers } = useMetadataContext();
+
+  const isSpeaker = useMemo(() => {
+    return speakers.includes(clientID) || host.clientIDs.includes(clientID);
+  }, [speakers, host, clientID]);
+
   const screenShareHandler = async (
     event: React.MouseEvent<HTMLButtonElement>
   ) => {
     event.preventDefault();
+
+    if (!isSpeaker) return;
+
     try {
       if (screenCaptureActive) {
         stopScreenCapture();
@@ -32,6 +45,9 @@ export default function ButtonScreenShare() {
       aria-label={`Toggle screen share ${screenCaptureActive ? 'off' : 'on'}`}
       className="bg-zinc-700/70 hover:bg-zinc-600 active:bg-zinc-500"
       onClick={screenShareHandler}
+      isDisabled={!isSpeaker}
+      disabled={!isSpeaker}
+      aria-disabled={!isSpeaker}
     >
       {screenCaptureActive ? (
         <ScreenShareOffIcon width={20} height={20} />
