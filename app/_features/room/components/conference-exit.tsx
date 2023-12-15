0398ad84be90c3f21786d3fe485d6cba831bc969
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-12-15 19:07:28 +0700

    feat: conference exit component

---
+++ app/_features/room/components/conference-exit.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { Button } from '@nextui-org/react';
+import Header from '@/_shared/components/header/header';
+
+export default function ConferenceExit() {
+  return (
+    <div className="min-viewport-height mx-auto flex w-full max-w-5xl flex-col px-4">
+      <Header logoText="inLive Room" logoHref="/" />
+      <main className="flex-1 pt-8 text-center">
+        <h2 className="text-3xl font-medium md:text-4xl">You left the Room</h2>
+        <div className="mt-8 flex flex-col justify-center gap-4 md:flex-row">
+          <div className="md:order-2">
+            <Button
+              className="h-9 w-52 rounded-md bg-red-700 px-3 py-2 text-sm font-medium text-zinc-200 antialiased hover:bg-red-600 active:bg-red-500 md:w-auto"
+              onClick={() => window.location.reload()}
+            >
+              Rejoin to Room
+            </Button>
+          </div>
+          <div>
+            <Button
+              as={Link}
+              href="/"
+              className="h-9 w-52 rounded-md bg-zinc-800 px-3 py-2 text-sm font-medium antialiased  hover:bg-zinc-700 active:bg-zinc-600 md:w-auto"
+            >
+              Back to Homepage
+            </Button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
