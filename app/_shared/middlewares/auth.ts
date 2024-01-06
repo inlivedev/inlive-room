ffeca330929974d456923eef219c818d35681f14
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-01-06 08:02:34 +0700

    refactor: remove getclientauth file and catch error with sentry

--- app/_shared/middlewares/auth.ts
+++ app/_shared/middlewares/auth.ts
@@ -3,7 +3,10 @@ import {
   type NextMiddleware,
   type NextRequest,
 } from 'next/server';
-import { getClientAuth } from '@/_shared/utils/get-client-auth';
+import { cookies } from 'next/headers';
+import Sentry from '@sentry/nextjs';
+import { InternalApiFetcher } from '@/_shared/utils/fetcher';
+import type { AuthType } from '@/_shared/types/auth';
 
 export function withAuthMiddleware(middleware: NextMiddleware) {
   return async (request: NextRequest, event: NextFetchEvent) => {
@@ -14,14 +17,27 @@ export function withAuthMiddleware(middleware: NextMiddleware) {
     }
 
     if (response) {
-      try {
-        const clientAuthResponse = await getClientAuth();
-        const currentAuth = clientAuthResponse.data
-          ? clientAuthResponse.data
-          : null;
+      const cookie = cookies().toString();
+      const clientAuthResponse: AuthType.CurrentAuthInternalResponse =
+        await InternalApiFetcher.get('/api/auth/current', {
+          headers: {
+            cookie: cookie,
+          },
+          cache: 'no-cache',
+        }).catch((error) => {
+          Sentry.captureException(error, {
+            extra: {
+              message: 'API call error when trying to get current auth data',
+            },
+          });
+          console.error(error);
+        });
 
-        response.headers.set('user-auth', JSON.stringify(currentAuth));
-      } catch (error) {}
+      const currentAuth = clientAuthResponse.data
+        ? clientAuthResponse.data
+        : null;
+
+      response.headers.set('user-auth', JSON.stringify(currentAuth));
     }
 
     return response;
