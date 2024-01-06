0671b4a59f3525dfe2f7c3e3e2e795377611f58f
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-01-06 14:06:36 +0700

    feat: error capturing on room service
    error capturing on createClient, setClientName, createRoom, joinRoom

--- app/(server)/_features/room/service.ts
+++ app/(server)/_features/room/service.ts
@@ -1,7 +1,5 @@
 import { iRoomService } from './routes';
-
 import * as Sentry from '@sentry/nextjs';
-
 import { generateID } from '@/(server)/_shared/utils/generateid';
 import { serverSDK } from '@/(server)/_shared/utils/sdk';
 import { insertRoom, selectRoom } from './schema';
@@ -36,23 +34,29 @@ export class RoomService implements iRoomService {
     this._datachannels = ['chat', 'moderator'];
   }
 
-  async createClient(
-    roomId: string,
-    clientName: string,
-    clientID?: string
-  ): Promise<Participant> {
+  async createClient(roomId: string, clientName: string, clientID?: string) {
     if (!this._roomRepo.isPersistent()) {
       const clientResponse = await this._sdk.createClient(roomId, {
         clientName: clientName,
       });
 
-      if (clientResponse.code == 409) {
-        throw new Error('Unable to create client ID Client ID already exist');
+      if (clientResponse && clientResponse.code === 409) {
+        Sentry.captureMessage(
+          `Failed to create client ID. The client ID already exists`,
+          'error'
+        );
+        throw new Error(
+          'Failed to create client ID. The client ID already exists'
+        );
       }
 
-      if (clientResponse.code > 299) {
+      if (!clientResponse || !clientResponse.ok) {
+        Sentry.captureMessage(
+          `Failed to create client for the room ${roomId}.`,
+          'error'
+        );
         throw new Error(
-          'Failed to add client to the meeting room. Please try again later!'
+          'Failed to create client for the room. Please try again later!'
         );
       }
 
@@ -66,7 +70,11 @@ export class RoomService implements iRoomService {
     const roomData = await this._roomRepo.getRoomById(roomId);
 
     if (!roomData) {
-      throw new Error('room not found');
+      Sentry.captureMessage(
+        `Room not found when trying to create a client`,
+        'error'
+      );
+      throw new Error('Room not found');
     }
 
     const clientResponse = await this._sdk.createClient(roomData.id, {
@@ -74,13 +82,23 @@ export class RoomService implements iRoomService {
       clientName: clientName,
     });
 
-    if (clientResponse.code == 409) {
-      throw new Error('Unable to create client ID Client ID already exist');
+    if (clientResponse && clientResponse.code === 409) {
+      Sentry.captureMessage(
+        `Failed to create client ID. The client ID already exists`,
+        'error'
+      );
+      throw new Error(
+        'Failed to create client ID. The client ID already exists'
+      );
     }
 
-    if (clientResponse.code > 299) {
+    if (!clientResponse || !clientResponse.ok) {
+      Sentry.captureMessage(
+        `Failed to create client for the room ${roomId}.`,
+        'error'
+      );
       throw new Error(
-        'Failed to add client to the meeting room. Please try again later!'
+        'Failed to create client for the room. Please try again later!'
       );
     }
 
@@ -100,14 +118,16 @@ export class RoomService implements iRoomService {
   ): Promise<Participant> {
     const updateResp = await this._sdk.setClientName(roomID, clientID, name);
 
-    if (updateResp.code > 499) {
-      throw new Error(
-        'an error has occured on our side please try again later'
+    if (!updateResp || !updateResp.ok) {
+      Sentry.captureMessage(
+        `Failed to set a client name with ${name}.`,
+        'error'
       );
-    }
 
-    if (updateResp.code > 299) {
-      throw new Error(updateResp.message);
+      throw new Error(
+        updateResp.message ||
+          `An error has occured on our side. Please try again later!`
+      );
     }
 
     return {
@@ -134,6 +154,7 @@ export class RoomService implements iRoomService {
       }
 
       if (roomResp.code > 299) {
+        Sentry.captureMessage(`Failed to create a room ${roomID}.`, 'error');
         throw new Error('Error during creating room, please try again later');
       }
 
@@ -144,10 +165,10 @@ export class RoomService implements iRoomService {
           true
         );
 
-        if (!channelResponse.ok) {
+        if (!channelResponse || !channelResponse.ok) {
           Sentry.captureException(
             new Error(
-              `Room ${roomID} : failed to create ${datachannel} data channel`
+              `Failed to create ${datachannel} data channel with room ID ${roomID}`
             )
           );
         }
@@ -171,24 +192,30 @@ export class RoomService implements iRoomService {
 
         return room;
       } catch (error) {
-        const err = error as Error;
-        if (err.message.includes('duplicate key')) {
-          retries++;
-          continue;
-        } else {
-          throw err;
+        Sentry.captureException(error, {
+          extra: {
+            message: `Failed to add a room to the DB.`,
+          },
+        });
+
+        if (error instanceof Error) {
+          if (error.message.includes('duplicate key')) {
+            retries++;
+            continue;
+          } else {
+            throw error;
+          }
         }
       }
     }
 
-    // Error for logging
-    Sentry.captureException(
-      new Error('failed to create meeting room : too many retires')
+    Sentry.captureMessage(
+      `Failed to create a room. Has reached max retries: ${maxRetries} retries`,
+      'error'
     );
 
-    // Error message given to user
     throw new Error(
-      'Failed to create a unique room ID, please try again later'
+      'Failed to create a unique room ID. Please try again later!'
     );
   }
 
@@ -219,11 +246,10 @@ export class RoomService implements iRoomService {
     if (room && room.id && remoteRoom.code === 404) {
       const newRemoteRoom = await this._sdk.createRoom('', room.id);
 
-      if (!newRemoteRoom.ok) {
-        Sentry.captureException(
-          new Error(
-            `failed to create room, got ${newRemoteRoom.code} response code from the SDK`
-          )
+      if (!newRemoteRoom || !newRemoteRoom.ok) {
+        Sentry.captureMessage(
+          `Failed to create a room. ${newRemoteRoom.code} ${newRemoteRoom.message}`,
+          'error'
         );
         throw new Error(
           'Error occured during accessing room data, please try again later'
@@ -237,9 +263,10 @@ export class RoomService implements iRoomService {
           true
         );
 
-        if (!channelResponse.ok) {
-          Sentry.captureException(
-            new Error(`Room ${room.id} : failed to create chat data channel`)
+        if (!channelResponse || !channelResponse.ok) {
+          Sentry.captureMessage(
+            `Failed to create ${datachannel} data channel with room ID ${roomId}`,
+            'error'
           );
         }
       }
