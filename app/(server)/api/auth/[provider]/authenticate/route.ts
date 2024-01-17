97a4aa3e65051c602afe0882cb44a58563e857a7
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-01-17 13:21:50 +0700

    refactor: simplify auth flow (#126)
    * refactor : remove unsued shared auth module
    
    * refactor: authorize no longer be imported from auth util
    
    * refactor: authenticate route no longer import from auth util
    
    * refactor: get current auth use separate file
    
    * feat: add try catch to catch unexcepted error
    
    * fix: try catch on auth middleware
    
    * fix: add checking when the response is not ok

--- app/(server)/api/auth/[provider]/authenticate/route.ts
+++ app/(server)/api/auth/[provider]/authenticate/route.ts
@@ -1,5 +1,8 @@
-import { authenticate } from '@/(server)/_shared/utils/auth';
 import { NextResponse, type NextRequest } from 'next/server';
+import { cookies } from 'next/headers';
+import * as Sentry from '@sentry/nextjs';
+import { InliveApiFetcher } from '@/_shared/utils/fetcher';
+import type { AuthType } from '@/_shared/types/auth';
 
 const APP_ORIGIN = process.env.NEXT_PUBLIC_APP_ORIGIN || '';
 
@@ -7,47 +10,88 @@ export async function GET(
   request: NextRequest,
   { params }: { params: { provider: string } }
 ) {
-  const url = new URL(request.url);
-  const currentPath = APP_ORIGIN + url.pathname;
-  const codeParam = url.searchParams.get('code') || '';
-  const stateParam = url.searchParams.get('state') || '';
-  const stateCookie = request.cookies.get('state')?.value || '';
-  const pathnameCookie = request.cookies.get('pathname')?.value || '';
-  const provider = params.provider;
+  try {
+    const url = new URL(request.url);
+    const currentPath = APP_ORIGIN + url.pathname;
+    const codeParam = url.searchParams.get('code') || '';
+    const stateParam = url.searchParams.get('state') || '';
+    const stateCookie = request.cookies.get('state')?.value || '';
+    const pathnameCookie = request.cookies.get('pathname')?.value || '';
+    const provider = params.provider;
 
-  if (stateParam === stateCookie) {
-    const authResponse = await authenticate(provider, currentPath, codeParam);
+    if (stateParam === stateCookie) {
+      const body = {
+        code: codeParam,
+        redirectURI: currentPath,
+      };
 
-    if (authResponse.data.token) {
-      const response = NextResponse.redirect(`${APP_ORIGIN}${pathnameCookie}`, {
-        status: 307,
-      });
+      const authResponse: AuthType.AuthenticateExternalResponse =
+        await InliveApiFetcher.post(`/auth/${provider}/authenticate`, {
+          body: JSON.stringify(body),
+        });
 
-      const thirtyDays = 60 * 60 * 24 * 30;
+      if (!authResponse || !authResponse.ok) {
+        Sentry.captureMessage(
+          `API call error when trying to authenticate user. ${
+            authResponse?.message || ''
+          }`,
+          'error'
+        );
 
-      response.cookies.set({
-        name: 'token',
-        value: authResponse.data.token,
-        path: '/',
-        sameSite: 'lax',
-        httpOnly: true,
-        maxAge: thirtyDays,
-      });
+        throw new Error(
+          `Authentication error when trying to authenticate with ${provider} SSO`
+        );
+      }
 
-      return response;
+      if (!authResponse.data || !authResponse.data.token) {
+        Sentry.captureMessage(
+          `Authentication error. No token received from the server.`,
+          'error'
+        );
+        throw new Error(
+          `Authentication error. No token received from the server.`
+        );
+      } else {
+        const thirtyDays = 60 * 60 * 24 * 30;
+
+        cookies().set({
+          name: 'token',
+          value: authResponse.data.token,
+          path: '/',
+          sameSite: 'lax',
+          httpOnly: true,
+          maxAge: thirtyDays,
+        });
+
+        return NextResponse.redirect(`${APP_ORIGIN}${pathnameCookie}`, {
+          status: 307,
+        });
+      }
+    } else {
+      return NextResponse.json(
+        {
+          code: 403,
+          message:
+            'Forbidden to proceed further. Incorrect authentication state.',
+          ok: false,
+          data: null,
+        },
+        {
+          status: 403,
+        }
+      );
     }
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        code: 500,
+        message: `Authentication error. ${error.message || ''}`,
+        ok: false,
+        data: null,
+      },
+      {
+        status: 500,
+      }
+    );
   }
-
-  // If the authentication is not successful
-  return NextResponse.json(
-    {
-      code: 403,
-      message: 'Forbidden',
-      ok: false,
-      data: null,
-    },
-    {
-      status: 403,
-    }
-  );
 }
--- app/(server)/api/auth/current/route.ts
+++ app/(server)/api/auth/current/route.ts
@@ -1,41 +1,59 @@
 import { NextResponse, type NextRequest } from 'next/server';
-import { getCurrentAuthenticated } from '@/(server)/_shared/utils/auth';
+import { getCurrentAuthenticated } from '@/(server)/_shared/utils/get-current-authenticated';
 
 export async function GET(request: NextRequest) {
-  const token = request.cookies.get('token')?.value || '';
+  try {
+    const token = request.cookies.get('token')?.value || '';
 
-  if (!token) {
+    if (!token) {
+      return NextResponse.json(
+        {
+          code: 401,
+          message:
+            'Unauthorized. Credential is needed to continue proceed with the request.',
+          ok: false,
+          data: null,
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    const response = await getCurrentAuthenticated(token);
+
+    if (response.code === 403) {
+      return NextResponse.json(
+        {
+          code: response.code,
+          message: 'Forbidden. Credential is invalid',
+          ok: false,
+          data: null,
+        },
+        {
+          status: response.code,
+        }
+      );
+    }
+
+    if (!response.ok) {
+      throw new Error(`Unexpected error occurred. ${response.message || ''}}`);
+    }
+
+    return NextResponse.json(response, {
+      status: response.code,
+    });
+  } catch (error: any) {
     return NextResponse.json(
       {
-        code: 401,
-        message:
-          'Unauthorized. Credential is needed to continue proceed with the request.',
+        code: 500,
+        message: `Server cannot retrieve the auth data. ${error.message || ''}`,
         ok: false,
         data: null,
       },
       {
-        status: 401,
+        status: 500,
       }
     );
   }
-
-  const response = await getCurrentAuthenticated(token);
-
-  if (response.code === 403) {
-    return NextResponse.json(
-      {
-        code: response.code,
-        message: 'Forbidden. Credential is invalid',
-        ok: false,
-        data: null,
-      },
-      {
-        status: response.code,
-      }
-    );
-  }
-
-  return NextResponse.json(response, {
-    status: response.code,
-  });
 }
