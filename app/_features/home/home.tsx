6bf75086cf4486887e272b5690e9cc0a54df36f4
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-01-18 15:29:21 +0700

    fix: webinar alert only be displayed for non whitelist user

--- app/_features/home/home.tsx
+++ app/_features/home/home.tsx
@@ -1,7 +1,10 @@
+'use client';
 import Header from '@/_shared/components/header/header';
 import CreateRoom from '@/_features/home/create-room';
 import JoinRoom from '@/_features/home/join-room';
 import Footer from '@/_shared/components/footer/footer';
+import { whitelistFeature } from '@/_shared/utils/flag';
+import { useAuthContext } from '@/_shared/contexts/auth';
 
 const WebinarBetaAlert = () => {
   return (
@@ -20,6 +23,8 @@ const WebinarBetaAlert = () => {
 };
 
 export default function View() {
+  const { user } = useAuthContext();
+
   return (
     <div className="bg-zinc-900 text-zinc-200">
       <div className="min-viewport-height mx-auto flex h-full w-full max-w-5xl flex-1 flex-col  px-4">
@@ -28,17 +33,23 @@ export default function View() {
           <div className="flex w-full flex-col gap-10 py-10 md:flex-row md:py-20 lg:gap-20">
             <div>
               <CreateRoom />
-              <div className="mt-10 block md:hidden">
-                <WebinarBetaAlert />
-              </div>
+              {!whitelistFeature.includes('event') &&
+              !user?.whitelistFeature.includes('event') ? (
+                <div className="mt-10 block md:hidden">
+                  <WebinarBetaAlert />
+                </div>
+              ) : null}
             </div>
             <div className="mx-auto w-full max-w-[400px] md:max-w-[360px] lg:max-w-[400px]">
               <JoinRoom />
             </div>
           </div>
-          <div className="hidden md:block">
-            <WebinarBetaAlert />
-          </div>
+          {!whitelistFeature.includes('event') &&
+          !user?.whitelistFeature.includes('event') ? (
+            <div className="hidden md:block">
+              <WebinarBetaAlert />
+            </div>
+          ) : null}
         </main>
         <Footer />
       </div>
