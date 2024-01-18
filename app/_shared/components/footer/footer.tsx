189650b868083c68e0cae019e866eaa30c17e323
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-01-18 10:32:19 +0700

    fix: fix responsive styling

--- app/_shared/components/footer/footer.tsx
+++ app/_shared/components/footer/footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="flex flex-col gap-5 py-6 lg:py-12">
+    <footer className="flex flex-col gap-5 py-6 md:py-12">
       <hr className="border-t border-zinc-700" />
-      <div className="mx-auto inline-flex items-center text-xs text-zinc-400">
+      <div className="mx-auto inline-flex flex-col items-center text-xs text-zinc-400 md:flex-row">
         <a
           href="https://inlive.app/"
           target="_blank"
@@ -11,24 +11,26 @@ export default function Footer() {
           <span>&copy; {new Date().getFullYear()}&nbsp;</span>
           <span>inLive</span>
         </a>
-        <span>&ensp;•&ensp;</span>
-        <a
-          href="https://github.com/inlivedev/inlive-room/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline-offset-4 hover:underline"
-        >
-          View code on GitHub
-        </a>
-        <span>&ensp;•&ensp;</span>
-        <a
-          href="https://twitter.com/inliveapp/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline-offset-4 hover:underline"
-        >
-          Follow for updates
-        </a>
+        <span className="hidden md:inline">&ensp;•&ensp;</span>
+        <div className="mt-2 inline-flex md:mt-0">
+          <a
+            href="https://github.com/inlivedev/inlive-room/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline-offset-4 hover:underline"
+          >
+            View code on GitHub
+          </a>
+          <span>&ensp;•&ensp;</span>
+          <a
+            href="https://twitter.com/inliveapp/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline-offset-4 hover:underline"
+          >
+            Follow for updates
+          </a>
+        </div>
       </div>
     </footer>
   );
