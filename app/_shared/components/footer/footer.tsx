3b194d69d7f191e98b48d5ec4b21a09ea328e420
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-01-18 10:21:55 +0700

    style: update footer styles

--- app/_shared/components/footer/footer.tsx
+++ app/_shared/components/footer/footer.tsx
@@ -2,21 +2,34 @@ export default function Footer() {
   return (
     <footer className="flex flex-col gap-5 py-6 lg:py-12">
       <hr className="border-t border-zinc-700" />
-      <p className="text-center text-xs text-zinc-400">
-        &copy; {new Date().getFullYear()}{' '}
-        <a href="https://inlive.app" target="_blank">
-          inLive
-        </a>{' '}
-        &nbsp;—&nbsp; The source code is available on{' '}
+      <div className="mx-auto inline-flex items-center text-xs text-zinc-400">
+        <a
+          href="https://inlive.app/"
+          target="_blank"
+          className="underline-offset-4 hover:underline"
+        >
+          <span>&copy; {new Date().getFullYear()}&nbsp;</span>
+          <span>inLive</span>
+        </a>
+        <span>&ensp;•&ensp;</span>
         <a
           href="https://github.com/inlivedev/inlive-room/"
           target="_blank"
           rel="noopener noreferrer"
-          className="underline underline-offset-4 hover:no-underline"
+          className="underline-offset-4 hover:underline"
         >
-          GitHub
+          View code on GitHub
         </a>
-      </p>
+        <span>&ensp;•&ensp;</span>
+        <a
+          href="https://twitter.com/inliveapp/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline-offset-4 hover:underline"
+        >
+          Follow for updates
+        </a>
+      </div>
     </footer>
   );
 }
