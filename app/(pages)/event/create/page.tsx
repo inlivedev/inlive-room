baa216dd7f672e6a10d60bada66ffd43e4355f46
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-01-31 15:06:36 +0700

    feat: title for create event page

--- app/(pages)/event/create/page.tsx
+++ app/(pages)/event/create/page.tsx
@@ -1,8 +1,13 @@
+import type { Metadata } from 'next';
 import { headers } from 'next/headers';
 import AppContainer from '@/_shared/components/containers/app-container';
 import EventForm from '@/_features/event/components/event-form';
 import { AuthType } from '@/_shared/types/auth';
 import HTTPError from '@/_shared/components/errors/http-error';
+
+export const metadata: Metadata = {
+  title: 'Create Your Event — inLive Room',
+};
 
 export default async function Page() {
   const headersList = headers();
