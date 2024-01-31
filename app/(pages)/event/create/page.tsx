7c9946511cbc67deaeae121f6a1f4d4d4935bc88
gagahgk <gagah.khaesarrahman@asumsi.co>
2024-01-31 07:52:30 +0700

    feat: limit access to create event page

--- app/(pages)/event/create/page.tsx
+++ app/(pages)/event/create/page.tsx
@@ -2,6 +2,7 @@ import { headers } from 'next/headers';
 import AppContainer from '@/_shared/components/containers/app-container';
 import EventForm from '@/_features/event/components/event-form';
 import { AuthType } from '@/_shared/types/auth';
+import HTTPError from '@/_shared/components/errors/http-error';
 
 export default async function Page() {
   const headersList = headers();
@@ -14,7 +15,15 @@ export default async function Page() {
 
   return (
     <AppContainer user={user}>
-      <EventForm></EventForm>
+      {user ? (
+        <EventForm />
+      ) : (
+        <HTTPError
+          code={403}
+          title="Login Required"
+          description="You need to be logged in to access this page"
+        />
+      )}
     </AppContainer>
   );
 }
