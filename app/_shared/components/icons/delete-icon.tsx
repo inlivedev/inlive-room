6eaddca3a3e1e4520f473652a71cbe39cd33fd29
gagahgk <gagah.khaesarrahman@asumsi.co>
2024-02-05 12:14:34 +0700

    fix: error on various icon

--- app/_shared/components/icons/delete-icon.tsx
+++ app/_shared/components/icons/delete-icon.tsx
@@ -5,7 +5,7 @@ export default function DeleteIcon(props: SVGElementPropsType) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      stroke-width="2"
+      strokeWidth="2"
       stroke="currentColor"
       fill="none"
       stroke-linecap="round"
