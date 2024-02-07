c81bca41cf41da6dd9bda19caab409ab487111da
gagahgk <gagah.khaesarrahman@asumsi.co>
2024-02-07 08:21:38 +0700

    chore: prepare for latest main migration

--- migrations/0012_on_delete_event_cascade.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "events_to_participant" DROP CONSTRAINT "events_to_participant_event_id_events_id_fk";
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "events_to_participant" ADD CONSTRAINT "events_to_participant_event_id_events_id_fk" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE cascade ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
