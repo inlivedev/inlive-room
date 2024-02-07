40ea4d65fccf4bf492a8e2e484a522debf5f2894
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-02-07 17:11:01 +0700

    feat: adjust meta data based on restriction conditions

--- app/(pages)/event/page.tsx
+++ app/(pages)/event/page.tsx
@@ -8,8 +8,36 @@ import { EventType } from '@/_shared/types/event';
 import { InternalApiFetcher } from '@/_shared/utils/fetcher';
 import { whitelistFeature } from '@/_shared/utils/flag';
 
-export const metadata: Metadata = {
-  title: 'My Events — inLive Room',
+export const metadata = (): Metadata => {
+  const headersList = headers();
+  const userAuthHeader = headersList.get('user-auth');
+
+  const user: AuthType.CurrentAuthData | null =
+    typeof userAuthHeader === 'string'
+      ? JSON.parse(userAuthHeader)
+      : userAuthHeader;
+
+  if (!user || !user.id) {
+    return {
+      title: `Login Required — inLive Room`,
+      description: 'You need to be logged in to access this page',
+    };
+  }
+
+  const eligibleForEvent =
+    whitelistFeature.includes('event') ||
+    !!user?.whitelistFeature.includes('event');
+
+  if (!eligibleForEvent) {
+    return {
+      title: `You are not eligible to see this page — inLive Room`,
+      description: 'Only early-access users can access this page.',
+    };
+  }
+
+  return {
+    title: `My Events — inLive Room`,
+  };
 };
 
 export default async function Page({
