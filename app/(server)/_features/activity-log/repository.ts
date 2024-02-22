b12e6553da99ad9dc53a202544d0d9bea64133af
Gagah Ghaniswara K <gagah.ghaniswara51@hotmail.com>
2024-02-22 12:00:50 +0700

    feat: function to aggregate the roomDuration

--- app/(server)/_features/activity-log/repository.ts
+++ app/(server)/_features/activity-log/repository.ts
@@ -1,6 +1,26 @@
 import { db } from '@/(server)/_shared/database/database';
 import { activitiesLog, type InsertActivityLog } from './schema';
+import { RoomType } from '@/_shared/types/room';
+import { sql, type SQL } from 'drizzle-orm';
 
 export const addLog = async (data: InsertActivityLog) => {
   return db.insert(activitiesLog).values(data).returning();
+};
+
+export const aggregateRoomDuration = async (
+  userID: number,
+  roomType?: RoomType.Type
+) => {
+  const sqlChunks: SQL[] = [];
+
+  sqlChunks.push(sql`${activitiesLog.createdBy} = ${userID} `);
+
+  if (roomType) {
+    sqlChunks.push(sql`AND ${activitiesLog.meta} ->> ${roomType}`);
+  }
+  const finalSQL = sql.join(sqlChunks);
+
+  const res = await db.select().from(activitiesLog).where(finalSQL);
+
+  console.log(res);
 };
