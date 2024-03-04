a96a5ca14f145dc3f97d672464b99e5e5026d323
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-03-04 13:34:32 +0700

    feat: make event create page publicly available

--- app/(pages)/event/create/page.tsx
+++ app/(pages)/event/create/page.tsx
@@ -2,37 +2,9 @@ import type { Metadata } from 'next';
 import { headers } from 'next/headers';
 import AppContainer from '@/_shared/components/containers/app-container';
 import EventForm from '@/_features/event/components/event-form';
-import { AuthType } from '@/_shared/types/auth';
-import HTTPError from '@/_shared/components/errors/http-error';
-import { whitelistFeature } from '@/_shared/utils/flag';
+import type { AuthType } from '@/_shared/types/auth';
 
 export const generateMetadata = (): Metadata => {
-  const headersList = headers();
-  const userAuthHeader = headersList.get('user-auth');
-
-  const user: AuthType.CurrentAuthData | null =
-    typeof userAuthHeader === 'string'
-      ? JSON.parse(userAuthHeader)
-      : userAuthHeader;
-
-  if (!user || !user.id) {
-    return {
-      title: `Login Required — inLive Room`,
-      description: 'You need to be logged in to access this page',
-    };
-  }
-
-  const eligibleForEvent =
-    whitelistFeature.includes('event') ||
-    !!user?.whitelistFeature.includes('event');
-
-  if (!eligibleForEvent) {
-    return {
-      title: `You are not eligible to see this page — inLive Room`,
-      description: 'Only early-access users can access this page.',
-    };
-  }
-
   return {
     title: `Create Your Event — inLive Room`,
   };
@@ -46,30 +18,6 @@ export default async function Page() {
     typeof userAuthHeader === 'string'
       ? JSON.parse(userAuthHeader)
       : userAuthHeader;
-
-  if (!user) {
-    return (
-      <HTTPError
-        title="Login Required"
-        description="You need to be logged in to access this page"
-      />
-    );
-  }
-
-  const eligibleForEvent =
-    whitelistFeature.includes('event') ||
-    !!user?.whitelistFeature.includes('event');
-
-  if (!eligibleForEvent) {
-    return (
-      <AppContainer user={user}>
-        <HTTPError
-          title="You are not eligible to see this page"
-          description="Only early-access users can access this page."
-        />
-      </AppContainer>
-    );
-  }
 
   return (
     <AppContainer user={user}>
