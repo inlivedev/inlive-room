e46f49b79a69c95b30458f15751e7b3f413ad207
gagahgk <20636513+GagahGK@users.noreply.github.com>
2024-03-07 16:43:32 +0700

    fix: migration script

--- migrations/0017_add_event_status.sql
+++ migrations/0017_add_event_status.sql
@@ -4,11 +4,12 @@ EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
-ALTER TABLE "events" ADD COLUMN "status" "event_status_enum" DEFAULT 'draft' NOT NULL;--> statement-breakpoint
+ALTER TABLE "events" ADD COLUMN IF NOT EXISTS "status" "event_status_enum" DEFAULT 'draft' NOT NULL;--> statement-breakpoint
 
+UPDATE "events" 
 SET status = CASE
-                WHEN is_published = false THEN 'draft'::status_enum
-                WHEN is_published = true THEN 'published'::status_enum
+                WHEN is_published = false THEN 'draft'::event_status_enum
+                WHEN is_published = true THEN 'published'::event_status_enum
             END;
 
 ALTER TABLE "events" DROP COLUMN IF EXISTS "is_published";
