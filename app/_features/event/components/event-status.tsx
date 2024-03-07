a579c56ad20ace928e892448960b443a97fbc262
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-03-07 20:23:33 +0700

    fix: outline 1px width for status badge

--- app/_features/event/components/event-status.tsx
+++ app/_features/event/components/event-status.tsx
@@ -1,20 +1,20 @@
 export function StatusPublished() {
   return (
-    <div className="inline-flex items-center rounded-sm bg-emerald-950 px-2 py-0.5 text-xs font-medium tracking-[0.275px] text-emerald-300 outline outline-emerald-800">
+    <div className="inline-flex items-center rounded-sm bg-emerald-950 px-2 py-0.5 text-xs font-medium tracking-[0.275px] text-emerald-300 outline outline-1 outline-emerald-800">
       Published
     </div>
   );
 }
 export function StatusDraft() {
   return (
-    <div className="inline-flex items-center rounded-sm bg-blue-950 px-2 py-0.5 text-xs font-medium tracking-[0.275px] text-blue-300 outline outline-blue-800">
+    <div className="inline-flex items-center rounded-sm bg-blue-950 px-2 py-0.5 text-xs font-medium tracking-[0.275px] text-blue-300 outline outline-1 outline-blue-800">
       Draft
     </div>
   );
 }
 export function StatusCancelled() {
   return (
-    <div className="inline-flex items-center rounded-sm bg-red-950 px-2 py-0.5 text-xs font-medium tracking-[0.275px] text-red-300 outline outline-red-800">
+    <div className="inline-flex items-center rounded-sm bg-red-950 px-2 py-0.5 text-xs font-medium tracking-[0.275px] text-red-300 outline outline-1 outline-red-800">
       Canceled
     </div>
   );
