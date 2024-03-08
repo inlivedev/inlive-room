1cd57f9d37d30fcc8a730fa3695c41f198f596d8
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-03-08 08:29:59 +0700

    feat: meta data and og image for my events and create event pages

--- app/(pages)/events/create/page.tsx
+++ app/(pages)/events/create/page.tsx
@@ -7,10 +7,25 @@ import { EventType } from '@/_shared/types/event';
 import { InternalApiFetcher } from '@/_shared/utils/fetcher';
 import { whitelistFeature } from '@/_shared/utils/flag';
 
-export const generateMetadata = (): Metadata => {
-  return {
-    title: `Let's Create Your Event — inLive Room`,
-  };
+const title = `Let's Create Your Event — inLive Event`;
+const description = `Enjoy features like analytics, hassle-free event registration, and automatic virtual room integration all conveniently accessible within a single application.`;
+const ogImage = '/images/webinar/og-image-webinar.png';
+
+export const metadata: Metadata = {
+  title: title,
+  openGraph: {
+    title: title,
+    description: description,
+    url: `/events/create`,
+    images: [ogImage],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: title,
+    description: description,
+    images: [ogImage],
+  },
 };
 
 export default async function Page() {
