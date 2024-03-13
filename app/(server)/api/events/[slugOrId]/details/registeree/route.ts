6d9fe4f0a0fca56a306cab6dc0e691a08fc558d5
Gagah Ghaniswara K <gagah.ghaniswara51@hotmail.com>
2024-03-13 16:39:27 +0700

    feat: endpoint to get registered participants (#176)
    * feat: endpoint to get event participants
    
    * Refactor GET route to include pagination parameters
    
    * add created_at to response

---
+++ app/(server)/api/events/[slugOrId]/details/registeree/route.ts
@@ -0,0 +1,69 @@
+import { getCurrentAuthenticated } from '@/(server)/_shared/utils/get-current-authenticated';
+import { eventRepo } from '@/(server)/api/_index';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+import * as Sentry from '@sentry/nextjs';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { slugOrId: string } }
+) {
+  const slugOrId = params.slugOrId;
+  const cookieStore = cookies();
+  const requestToken = cookieStore.get('token');
+  const { searchParams } = new URL(request.url);
+  const page = parseInt(searchParams.get('page') ?? '1');
+  const limit = parseInt(searchParams.get('limit') ?? '10');
+
+  try {
+    if (!requestToken) {
+      return NextResponse.json(
+        {
+          code: 401,
+          message: 'please check your authentication token',
+        },
+        { status: 401 }
+      );
+    }
+
+    const response = await getCurrentAuthenticated(requestToken?.value);
+    const user = response.data ? response.data : null;
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          code: 401,
+          message: 'unauthorized',
+        },
+        { status: 401 }
+      );
+    }
+
+    const res = await eventRepo.getRegisteredParticipants(
+      slugOrId,
+      user.id,
+      limit,
+      page
+    );
+
+    return NextResponse.json(
+      {
+        data: res.data,
+        meta: res.meta,
+        message: 'success',
+        ok: true,
+      },
+      { status: 200 }
+    );
+  } catch (e) {
+    Sentry.captureException(e);
+    return NextResponse.json(
+      {
+        code: 500,
+        message: 'internal server error',
+        ok: false,
+      },
+      { status: 500 }
+    );
+  }
+}
