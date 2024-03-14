02f40a180e8b2c074b73fa46407b0abcacf68b65
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-03-14 20:08:55 +0700

    fix: not centered back to homepage on mobile

--- app/_features/room/components/conference-exit.tsx
+++ app/_features/room/components/conference-exit.tsx
@@ -7,7 +7,7 @@ export default function ConferenceExit() {
       <Header logoText="inLive Room" logoHref="/" />
       <main className="flex-1 pt-8 text-center">
         <h2 className="text-3xl font-medium md:text-4xl">You left the Room</h2>
-        <div className="mt-8 flex flex-col justify-center gap-4 md:flex-row">
+        <div className="mt-8 flex flex-col items-center justify-center gap-4 md:flex-row">
           <div className="md:order-2">
             <Button
               className="h-9 w-52 rounded-md bg-red-700 px-3 py-2 text-sm font-medium text-zinc-200 antialiased hover:bg-red-600 active:bg-red-500 md:w-auto"
