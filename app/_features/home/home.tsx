66b427d571ab496a350e2b35638df17c48a5bbc6
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-03-16 17:56:11 +0700

    fix: update max width layout for home

--- app/_features/home/home.tsx
+++ app/_features/home/home.tsx
@@ -7,15 +7,17 @@ import Footer from '@/_shared/components/footer/footer';
 export default function View() {
   return (
     <div className="bg-zinc-900 text-zinc-200">
-      <div className="min-viewport-height mx-auto flex h-full w-full max-w-5xl flex-1 flex-col  px-4">
+      <div className="min-viewport-height mx-auto flex h-full w-full max-w-7xl flex-1 flex-col px-4">
         <Header logoText="inLive Room" logoHref="/" />
         <main className="flex flex-1 flex-col justify-center">
           <div className="flex w-full flex-col gap-10 py-10 md:flex-row md:py-20 lg:gap-20">
-            <div>
+            <div className="flex-auto">
               <CreateRoom />
             </div>
-            <div className="mx-auto w-full max-w-[400px] md:max-w-[360px] lg:max-w-[400px]">
-              <JoinRoom />
+            <div className="flex flex-1 justify-center md:justify-end">
+              <div className="w-[400px] md:w-[360px] lg:w-[400px]">
+                <JoinRoom />
+              </div>
             </div>
           </div>
         </main>
