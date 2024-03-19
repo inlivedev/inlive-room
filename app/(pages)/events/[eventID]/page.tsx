0acccb1fd13f589d96694ccf0f368af181b7ea49
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-03-19 14:36:23 +0700

    fix: make this event detail page for public use

--- app/(pages)/events/[eventID]/page.tsx
+++ app/(pages)/events/[eventID]/page.tsx
@@ -1,12 +1,10 @@
 import type { Metadata } from 'next';
-import { cookies, headers } from 'next/headers';
+import { headers } from 'next/headers';
 import { notFound } from 'next/navigation';
 import AppContainer from '@/_shared/components/containers/app-container';
 import EventDetail from '@/_features/event/components/event-detail';
-import EventDetailDashboard from '@/_features/event/components/event-detail-dashboard';
-import { InternalApiFetcher } from '@/_shared/utils/fetcher';
+import { eventService } from '@/(server)/api/_index';
 import type { AuthType } from '@/_shared/types/auth';
-import type { EventType } from '@/_shared/types/event';
 
 type PageProps = {
   params: {
@@ -40,42 +38,34 @@ function sanitizeHTML(htmlString: string) {
 export const generateMetadata = async ({
   params: { eventID },
 }: PageProps): Promise<Metadata> => {
-  const cookie = (await cookies().get('token')?.value) ?? '';
+  const event = await eventService.getEventBySlugOrID(eventID, undefined);
 
-  const { data: eventData }: EventType.DetailEventResponse =
-    await InternalApiFetcher.get(`/api/events/${eventID}`, {
-      headers: {
-        Cookie: `token=${cookie}`,
-      },
-      cache: 'no-cache',
-    });
-
-  if (!eventData || !eventData.id) {
+  if (!event || !event.id) {
     return {
       title: 'Page Not Found',
       description: 'There is nothing to see on this page',
     };
   }
 
-  const imageSrc = eventData.thumbnailUrl
-    ? `${APP_ORIGIN}/static${eventData.thumbnailUrl}`
+  const imageSrc = event.thumbnailUrl
+    ? `${APP_ORIGIN}/static${event.thumbnailUrl}`
     : '/images/webinar/og-image-inlive-room-webinar-generic-en.png';
-  const description = sanitizeHTML(eventData.description || '');
+  const description = sanitizeHTML(event.description || '');
   const descriptionSummary = `${description.slice(0, 150) + '...'}`;
 
   return {
-    title: `Webinar — ${eventData.name} — inLive Room`,
+    title: `Webinar — ${event.name} — inLive Room`,
     description: descriptionSummary,
     openGraph: {
-      title: `Webinar — ${eventData.name} — inLive Room`,
+      title: `Webinar — ${event.name} — inLive Room`,
       description: descriptionSummary,
-      url: `/events/${eventData.slug}`,
+      url: `/events/${event.slug}`,
       images: [imageSrc],
       type: 'website',
     },
     twitter: {
       card: 'summary_large_image',
-      title: `Webinar — ${eventData.name} — inLive Room`,
+      title: `Webinar — ${event.name} — inLive Room`,
       description: descriptionSummary,
       images: [imageSrc],
     },
@@ -86,59 +76,20 @@ export default async function Page({ params: { eventID } }: PageProps) {
   const headersList = headers();
   const userAuthHeader = headersList.get('user-auth');
 
-  const userAuth: AuthType.CurrentAuthData | null =
+  const user: AuthType.CurrentAuthData | null =
     typeof userAuthHeader === 'string'
       ? JSON.parse(userAuthHeader)
       : userAuthHeader;
 
-  const cookie = cookies().get('token')?.value ?? '';
+  const event = await eventService.getEventBySlugOrID(eventID, undefined);
 
-  const { data: eventData }: EventType.DetailEventResponse =
-    await InternalApiFetcher.get(`/api/events/${eventID}`, {
-      headers: {
-        Cookie: `token=${cookie}`,
-      },
-      cache: 'no-cache',
-    });
-
-  if (!eventData || !eventData.id) {
-    notFound();
-  }
-
-  const isHost = userAuth?.id === eventData.createdBy;
-
-  let registerees: EventType.RegistereeParticipant[] = [];
-  let totalRegisterees = 0;
-
-  if (isHost && eventData.status === 'published') {
-    const registereesResponse: EventType.RegistereeParticipantResponse =
-      await InternalApiFetcher.get(
-        `/api/events/${eventID}/details/registeree`,
-        {
-          headers: {
-            Cookie: `token=${cookie}`,
-          },
-        }
-      );
-
-    registerees = registereesResponse.data;
-    totalRegisterees = registereesResponse.meta.total_record;
+  if (!event) {
+    return notFound();
   }
 
   return (
-    <AppContainer user={userAuth}>
-      {(isHost && eventData.status === 'published') ||
-      eventData.status === 'cancelled' ? (
-        <EventDetailDashboard
-          event={eventData}
-          registerees={registerees}
-          totalRegisterees={totalRegisterees}
-        />
-      ) : isHost && eventData.status === 'draft' ? (
-        <EventDetail event={eventData} status="draft" />
-      ) : (
-        <EventDetail event={eventData} status="public" />
-      )}
+    <AppContainer user={user}>
+      <EventDetail event={event} status="public" />
     </AppContainer>
   );
 }
