f54965894cabd2ad635e9ea8381936bb5f908ab5
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-03-21 14:19:30 +0700

    fix: using state instead of memo for date formatting

--- app/_shared/hooks/use-formatted-datetime.ts
+++ app/_shared/hooks/use-formatted-datetime.ts
@@ -1,14 +1,18 @@
-import { useMemo } from 'react';
+import { useState, useEffect } from 'react';
 
 export const useFormattedDateTime = (
   date: Date,
   locales: string | string[] | undefined,
   options: Intl.DateTimeFormatOptions | undefined
 ) => {
-  const formattedDateTime = useMemo(
-    () => new Intl.DateTimeFormat(locales, options).format(new Date(date)),
-    [date, locales, options]
-  );
+  const [formattedDate, setFormattedDate] = useState<string>('');
 
-  return formattedDateTime;
+  useEffect(() => {
+    const formatted = new Intl.DateTimeFormat(locales, options).format(
+      new Date(date)
+    );
+    setFormattedDate(formatted);
+  }, [date, locales, options]);
+
+  return formattedDate;
 };
