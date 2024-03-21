eeafe01611c1785c96634d99ae8bd5af2db9882a
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-03-21 13:17:54 +0700

    refactor: back rendering the date in the client

--- app/_features/event/components/event-detail-dashboard.tsx
+++ app/_features/event/components/event-detail-dashboard.tsx
@@ -23,32 +23,39 @@ import { webShare } from '@/_shared/utils/web-share';
 import { useToggle } from '@/_shared/hooks/use-toggle';
 import CancelEventModal from './event-cancel-modal';
 import type { SVGElementPropsType } from '@/_shared/types/types';
+import { useFormattedDateTime } from '@/_shared/hooks/use-formatted-datetime';
 
 const APP_ORIGIN = process.env.NEXT_PUBLIC_APP_ORIGIN;
-
-type Registree = {
-  id: number;
-  name: string;
-  email: string;
-  registeredAt: string;
-};
 
 export default function EventDetailDashboard({
   event,
   registerees,
-  startDateWithYear,
-  startTime,
-  createdDate,
 }: {
   event: EventType.Event;
-  registerees: Registree[];
-  startDateWithYear: string;
-  startTime: string;
-  createdDate: string;
+  registerees: EventType.RegistereeParticipant[];
 }) {
   const thumbnailUrl = event.thumbnailUrl
     ? `${APP_ORIGIN}/static${event.thumbnailUrl}`
     : '/images/webinar/webinar-no-image-placeholder.png';
+
+  const startDateWithYear = useFormattedDateTime(event.startTime, 'en-GB', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+
+  const startTime = useFormattedDateTime(event.startTime, 'en-US', {
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  });
+
+  const createdDate = useFormattedDateTime(event.createdAt, 'en-GB', {
+    month: 'long',
+    day: '2-digit',
+    year: 'numeric',
+  });
 
   return (
     <div className="bg-zinc-900">
@@ -208,20 +215,10 @@ export default function EventDetailDashboard({
                       {registerees.length > 0 ? (
                         registerees.map((registeree) => {
                           return (
-                            <tr key={`${registeree.id}-${registeree.email}`}>
-                              <th
-                                scope="row"
-                                className="min-w-52 max-w-80 truncate whitespace-nowrap p-3 font-medium text-zinc-200 lg:p-6"
-                              >
-                                {registeree.name}
-                              </th>
-                              <td className="min-w-52 max-w-80 truncate whitespace-nowrap p-3 text-zinc-400 lg:p-6">
-                                {registeree.email}
-                              </td>
-                              <td className="min-w-52 max-w-80 truncate whitespace-nowrap p-3 text-zinc-400 lg:p-6">
-                                {registeree.registeredAt}
-                              </td>
-                            </tr>
+                            <RegistereeItem
+                              key={`${registeree.id}-${registeree.email}`}
+                              registeree={registeree}
+                            />
                           );
                         })
                       ) : (
@@ -247,6 +244,45 @@ export default function EventDetailDashboard({
         </div>
       </div>
     </div>
+  );
+}
+
+function RegistereeItem({
+  registeree,
+}: {
+  registeree: EventType.RegistereeParticipant;
+}) {
+  const name = `${registeree.firstName} ${registeree.lastName}`;
+
+  const registeredDate = useFormattedDateTime(registeree.createdAt, 'en-GB', {
+    month: 'long',
+    day: '2-digit',
+    year: 'numeric',
+  });
+
+  const registeredTime = useFormattedDateTime(registeree.createdAt, 'en-US', {
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  });
+
+  const registeredAt = `${registeredDate}, ${registeredTime}`;
+
+  return (
+    <tr>
+      <th
+        scope="row"
+        className="min-w-52 max-w-80 truncate whitespace-nowrap p-3 font-medium text-zinc-200 lg:p-6"
+      >
+        {name}
+      </th>
+      <td className="min-w-52 max-w-80 truncate whitespace-nowrap p-3 text-zinc-400 lg:p-6">
+        {registeree.email}
+      </td>
+      <td className="min-w-52 max-w-80 truncate whitespace-nowrap p-3 text-zinc-400 lg:p-6">
+        {registeredAt}
+      </td>
+    </tr>
   );
 }
 
