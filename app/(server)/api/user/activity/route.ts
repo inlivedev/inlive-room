e6c43f88f15627dd598b429dad3e82bc7bc82c02
gagahgk <20636513+GagahGK@users.noreply.github.com>
2024-03-27 14:31:23 +0700

    refactor: convert date string into date during parse

--- app/(server)/api/user/activity/route.ts
+++ app/(server)/api/user/activity/route.ts
@@ -10,16 +10,24 @@ const ActivityRequest = z.object({
   meta: z.any(),
 });
 
-const RoomDurationMeta = z.object({
+export const RoomDurationMeta = z.object({
   roomID: z.string(),
   clientID: z.string(),
   name: z.string().optional(),
-  joinTime: z.string().datetime({ offset: true }),
-  leaveTime: z.string().datetime({ offset: true }),
+  joinTime: z
+    .string()
+    .datetime({ offset: true })
+    .transform((val) => new Date(val)),
+  leaveTime: z
+    .string()
+    .datetime({ offset: true })
+    .transform((val) => new Date(val)),
   roomType: z.enum(['meeting', 'event']),
-  duration: z.number().optional(),
+  duration: z.number().default(0),
   trigger: z.enum(['beforeunload', 'leave-button']),
 });
+
+export const ArrayRoomDurationMeta = z.array(RoomDurationMeta);
 
 const activityName = ['RoomDuration'];
 
@@ -58,11 +66,9 @@ export async function POST(request: NextRequest) {
       parsedMeta.duration = duration;
 
       // Adjust if time is not synchronized
-      if (!isWithinTolerance(new Date(parsedMeta.leaveTime), 5 * 60 * 1000)) {
-        parsedMeta.leaveTime = currentTime.toISOString();
-        parsedMeta.joinTime = new Date(
-          currentTime.getTime() - duration
-        ).toISOString();
+      if (isWithinTolerance(parsedMeta.leaveTime, 5 * 60 * 1000)) {
+        parsedMeta.leaveTime = currentTime;
+        parsedMeta.joinTime = new Date(currentTime.getTime() - duration);
       }
     }
 
