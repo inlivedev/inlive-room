e2d6d11b691ccf552a28569bdaedc13c655eb2ac
gagahgk <20636513+GagahGK@users.noreply.github.com>
2024-04-29 13:29:46 +0700

    feat: add the event host into participant table

--- migrations/0023_add_participant_role.sql
+++ migrations/0023_add_participant_role.sql
@@ -12,10 +12,17 @@ EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 
--- Todo : Migration 
-
 INSERT INTO "participant_role" ("id","name") VALUES (2,'host');
 INSERT INTO "participant_role" ("id","name") VALUES (1,'viewer');
-
-
 ALTER TABLE "events_participant" ALTER COLUMN "role_id" SET NOT NULL;
+
+-- Add Host into Participant Table
+INSERT INTO "events_participant" ("event_id","email","role_id","first_name","last_name","client_id") 
+SELECT e.id , u.email, 2, u.name, "" ,(LEFT(md5(gen_random_uuid()::text), 12))
+FROM "events" e
+JOIN "users" u ON e.created_by = u.id
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM event_participant ep
+    WHERE ep.email = u.email
+);
