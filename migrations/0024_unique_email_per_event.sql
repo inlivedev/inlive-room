7cc04d446f96daac40c7fd852b4b25f290e89a12
gagahgk <20636513+GagahGK@users.noreply.github.com>
2024-04-30 13:09:28 +0700

    feat: add unique constrain email per event only if not exist

--- migrations/0024_unique_email_per_event.sql
+++ migrations/0024_unique_email_per_event.sql
@@ -6,4 +6,12 @@ WHERE ep1.email = ep2.email
   AND ep1.event_id = ep2.event_id 
   AND ep1.id > ep2.id;
 
-ALTER TABLE "events_participant" ADD CONSTRAINT "events_participant_email_event_id_unique" UNIQUE("email","event_id");
+DO $$
+BEGIN
+    BEGIN
+        ALTER TABLE "events_participant" ADD CONSTRAINT "events_participant_email_event_id_unique" UNIQUE("email","event_id");
+    EXCEPTION WHEN duplicate_object THEN
+        -- handle the error
+    END;
+END;
+$$;
