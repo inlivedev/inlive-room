4ed6676392a96a3e422e4656a482049ed2855c36
gagahgk <20636513+GagahGK@users.noreply.github.com>
2024-04-30 13:09:13 +0700

    refactor: add column and it's contrain only if not exist

--- migrations/0023_add_participant_role.sql
+++ migrations/0023_add_participant_role.sql
@@ -2,20 +2,29 @@ CREATE TABLE IF NOT EXISTS "participant_role" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" text NOT NULL
 );
---> statement-breakpoint
-ALTER TABLE "events_participant" ADD COLUMN "role_id" integer;--> statement-breakpoint
-UPDATE "events_participant" SET "role_id" = 1 WHERE "role_id" IS NULL;
-
-DO $$ BEGIN
- ALTER TABLE "events_participant" ADD CONSTRAINT "events_participant_role_id_participant_role_id_fk" FOREIGN KEY ("role_id") REFERENCES "participant_role"("id") ON DELETE set null ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
 
 INSERT INTO "participant_role" ("id","name") VALUES (2,'host');
 INSERT INTO "participant_role" ("id","name") VALUES (1,'viewer');
-ALTER TABLE "events_participant" ALTER COLUMN "role_id" SET NOT NULL;
 
+--> statement-breakpoint
+DO $$
+BEGIN
+    BEGIN
+        ALTER TABLE "events_participant" ADD COLUMN "role_id" integer;
+    EXCEPTION WHEN duplicate_column THEN
+        -- handle the error
+    END;
+    BEGIN
+        ALTER TABLE "events_participant" ADD CONSTRAINT "events_participant_role_id_participant_role_id_fk" FOREIGN KEY ("role_id") REFERENCES "participant_role"("id") ON DELETE set null ON UPDATE no action;
+    EXCEPTION WHEN duplicate_object THEN
+        -- handle the error
+    END;
+END;
+$$;
+
+UPDATE "events_participant" SET "role_id" = 1 WHERE "role_id" IS NULL;
+
+ALTER TABLE "events_participant" ALTER COLUMN "role_id" SET NOT NULL;
 -- Add Host into Participant Table
 INSERT INTO "events_participant" ("event_id","email","role_id","first_name","last_name","client_id") 
 SELECT e.id , u.email, 2, u.name, '' ,(LEFT(md5(gen_random_uuid()::text), 12))
