3eddd02e027046a3b39e37196ad994025ba739d5
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-05-08 16:33:25 +0700

    refactor: remove user state

--- app/_shared/contexts/auth.tsx
+++ app/_shared/contexts/auth.tsx
@@ -1,19 +1,10 @@
 'use client';
 
-import {
-  createContext,
-  useContext,
-  useState,
-  type Dispatch,
-  type SetStateAction,
-} from 'react';
+import { createContext, useContext } from 'react';
 import type { AuthType } from '@/_shared/types/auth';
 
 const AuthContext = createContext({
   user: null as AuthType.CurrentAuthContext | null,
-  setUser: (() => {}) as Dispatch<
-    SetStateAction<AuthType.CurrentAuthContext | null>
-  >,
 });
 
 export const useAuthContext = () => {
@@ -27,12 +18,7 @@ export function AuthProvider({
   children: React.ReactNode;
   user: AuthType.CurrentAuthContext | null;
 }) {
-  const [userState, setUserState] =
-    useState<AuthType.CurrentAuthContext | null>(user);
-
   return (
-    <AuthContext.Provider value={{ user: userState, setUser: setUserState }}>
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
   );
 }
