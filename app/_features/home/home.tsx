461bfbd9196247bd22352192202b3a21c473519f
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-05-19 17:12:07 +0700

    feat: home with signed in and not signed in components

--- app/_features/home/home.tsx
+++ app/_features/home/home.tsx
@@ -1,47 +1,22 @@
 'use client';
 import Header from '@/_shared/components/header/header';
-import CreateRoom from '@/_features/home/create-room';
-import JoinRoom from '@/_features/home/join-room';
 import Footer from '@/_shared/components/footer/footer';
-import MeetingList from '../meeting/meeting-list';
+import SignedIn from '@/_features/home/signed-in';
+import NotSignedIn from '@/_features/home/not-signed-in';
+import { useAuthContext } from '@/_shared/contexts/auth';
 
 export default function View() {
+  const { user } = useAuthContext();
+
   return (
     <div className="bg-zinc-900 text-zinc-200">
       <div className="min-viewport-height mx-auto flex h-full w-full max-w-7xl flex-1 flex-col px-4">
         <Header logoText="inLive Room" logoHref="/" />
         <main className="flex flex-1 flex-col justify-center">
-          <div className="flex w-full flex-col items-center gap-10 py-10 md:flex-row md:py-20 lg:gap-20">
-            <div className="flex flex-auto basis-2/3 flex-col gap-8">
-              <Title />
-              <div className="flex flex-row gap-2">
-                <CreateRoom />
-                <JoinRoom />
-              </div>
-            </div>
-
-            <div className="w-[400px] basis-1/3 md:w-[360px] lg:w-[400px]">
-              <MeetingList />
-            </div>
-          </div>
+          {user ? <SignedIn /> : <NotSignedIn />}
         </main>
         <Footer />
       </div>
-    </div>
-  );
-}
-
-function Title() {
-  return (
-    <div>
-      <h2 className="text-center text-3xl font-semibold tracking-wide text-zinc-200 sm:text-left lg:text-4xl">
-        Virtual room for your real-time collaboration
-      </h2>
-      <p className="mt-4 text-pretty text-center text-base text-zinc-400 sm:text-left lg:text-lg">
-        Connect with anyone, anywhere. Host or join in seconds. It&apos;s that
-        simple! Experience real-time messaging, video, and audio for seamless
-        collaboration, all within open-source virtual rooms.
-      </p>
     </div>
   );
 }
---
+++ app/_features/home/not-signed-in.tsx
@@ -0,0 +1,44 @@
+'use client';
+import { Button } from '@nextui-org/react';
+import JoinRoomField from '@/_features/home/join-room-field';
+
+export default function NotSignedIn() {
+  const openSignInModal = () => {
+    document.dispatchEvent(new CustomEvent('open:sign-in-modal'));
+  };
+
+  return (
+    <div className="flex w-full flex-col gap-10 py-10 md:flex-row md:py-20 lg:gap-20">
+      <div className="flex-auto">
+        <section className="md:max-w-xl">
+          <h2 className="text-3xl font-semibold tracking-wide text-zinc-200 lg:text-4xl">
+            Virtual room for your real-time collaboration
+          </h2>
+          <p className="mt-4 text-pretty text-base text-zinc-400 lg:text-lg">
+            Connect with anyone, anywhere. Host or join in seconds. It&apos;s
+            that simple! Experience real-time messaging, video, and audio for
+            seamless collaboration, all within open-source virtual rooms.
+          </p>
+          <div className="mt-8">
+            <Button
+              className="w-52 rounded-md bg-red-700 px-6 py-2 text-sm font-medium text-zinc-200 antialiased hover:bg-red-600 active:bg-red-500"
+              onClick={openSignInModal}
+            >
+              Sign in to try inLive Room
+            </Button>
+          </div>
+        </section>
+      </div>
+      <div className="flex flex-1 justify-center md:justify-end">
+        <div className="w-[400px] md:w-[360px] lg:w-[400px]">
+          <div className="rounded-2xl border border-zinc-950 bg-zinc-950/25 p-6 lg:p-8">
+            <p className="mb-4 text-base font-medium text-zinc-400">
+              Got a room code to join?
+            </p>
+            <JoinRoomField />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
