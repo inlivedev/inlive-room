c088b008ed1c220a537c6265e936965aa045b71e
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-05-22 10:43:01 +0700

    feat: import schedule modal

--- app/_features/home/signed-in.tsx
+++ app/_features/home/signed-in.tsx
@@ -16,6 +16,7 @@ import { InternalApiFetcher } from '@/_shared/utils/fetcher';
 import JoinRoomField from '@/_features/home/join-room-field';
 import type { AuthType } from '@/_shared/types/auth';
 import MeetingList from '@/_features/meeting/meeting-list';
+import ScheduleModal from '@/_features/meeting/schedule-modal';
 
 const createRoom = async (type: string) => {
   const response: RoomType.CreateGetRoomResponse =
@@ -53,113 +54,126 @@ export default function SignedIn({
     async (key: Key) => {
       if (isSubmitting || typeof key !== 'string') return;
 
-      setIsSubmitting(true);
+      if (key === 'event' || key === 'meeting') {
+        try {
+          setIsSubmitting(true);
+          const room = await createRoom(key);
+          setIsSubmitting(false);
+          window.location.href = `/rooms/${room.id}`;
+        } catch (error) {
+          Sentry.captureException(error, {
+            extra: {
+              message: `API call error when trying to create ${key} room`,
+            },
+          });
 
-      try {
-        const room = await createRoom(key);
-        setIsSubmitting(false);
-        window.location.href = `/rooms/${room.id}`;
-      } catch (error) {
-        Sentry.captureException(error, {
-          extra: {
-            message: `API call error when trying to create ${key} room`,
-          },
-        });
+          setIsSubmitting(false);
+          alert('Failed to create a room. Please try again later! ');
+          console.error(error);
+        }
+      }
 
-        setIsSubmitting(false);
-        alert('Failed to create a room. Please try again later! ');
-        console.error(error);
+      if (key === 'schedule') {
+        document.dispatchEvent(new CustomEvent('open:schedule-meeting-modal'));
       }
     },
     [isSubmitting]
   );
 
   return (
-    <div className="grid w-full grid-cols-1 gap-y-12 md:grid-cols-2">
-      <div className="md:px-5 lg:px-10">
-        <h2 className="text-3xl font-semibold tracking-wide text-zinc-200 lg:text-4xl">
-          Hi, {firstName}.
-        </h2>
-        <p className="mt-3 text-pretty text-base text-zinc-400 lg:text-lg">
-          Get the conversation going in virtual room. Ready, set, collaborate!
-        </p>
-        <div className="mt-8">
-          <Dropdown
-            placement="bottom-start"
-            className="ring-1 ring-zinc-800/70"
-          >
-            <DropdownTrigger>
-              <Button
-                className="h-auto min-h-0 min-w-0 rounded-lg bg-red-700 px-6 py-2.5 text-sm font-medium text-zinc-100 antialiased hover:bg-red-600 active:bg-red-500"
-                isDisabled={isSubmitting}
-                aria-disabled={isSubmitting}
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? (
-                  <div className="flex gap-2">
-                    <Spinner
-                      classNames={{
-                        circle1: 'border-b-zinc-200',
-                        circle2: 'border-b-zinc-200',
-                        wrapper: 'w-4 h-4',
-                      }}
-                    />
-                    <span>Creating a room...</span>
-                  </div>
-                ) : (
-                  <span>New room</span>
-                )}
-              </Button>
-            </DropdownTrigger>
-            <DropdownMenu
-              disallowEmptySelection
-              aria-label="Create a room menu"
-              onAction={onCreateRoomSelection}
+    <>
+      <ScheduleModal />
+      <div className="grid w-full grid-cols-1 gap-y-12 md:grid-cols-2">
+        <div className="md:px-5 lg:px-10">
+          <h2 className="text-3xl font-semibold tracking-wide text-zinc-200 lg:text-4xl">
+            Hi, {firstName}.
+          </h2>
+          <p className="mt-3 text-pretty text-base text-zinc-400 lg:text-lg">
+            Get the conversation going in virtual room. Ready, set, collaborate!
+          </p>
+          <div className="mt-8">
+            <Dropdown
+              placement="bottom-start"
+              className="ring-1 ring-zinc-800/70"
             >
-              <DropdownItem
-                key="meeting"
-                classNames={{
-                  wrapper: 'group',
-                }}
+              <DropdownTrigger>
+                <Button
+                  className="h-auto min-h-0 min-w-0 rounded-lg bg-red-700 px-6 py-2.5 text-sm font-medium text-zinc-100 antialiased hover:bg-red-600 active:bg-red-500"
+                  isDisabled={isSubmitting}
+                  aria-disabled={isSubmitting}
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? (
+                    <div className="flex gap-2">
+                      <Spinner
+                        classNames={{
+                          circle1: 'border-b-zinc-200',
+                          circle2: 'border-b-zinc-200',
+                          wrapper: 'w-4 h-4',
+                        }}
+                      />
+                      <span>Creating a room...</span>
+                    </div>
+                  ) : (
+                    <span>New room</span>
+                  )}
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                disallowEmptySelection
+                aria-label="Create a room menu"
+                onAction={onCreateRoomSelection}
               >
-                <div className="text-sm font-medium text-zinc-200">
-                  <span className="inline-block">Meeting</span>
-                </div>
-                <div className="text-xs text-zinc-400 group-hover:text-zinc-200">
-                  Personal or group meetings
-                </div>
-              </DropdownItem>
-              <DropdownItem
-                key="event"
-                classNames={{
-                  wrapper: 'group',
-                }}
-              >
-                <div className="flex justify-between text-sm font-medium text-zinc-200">
-                  <span className="inline-block">Webinar</span>
-                  <div className="inline-flex items-center">
-                    <span className="rounded-sm border-1 border-emerald-800 bg-emerald-950 px-1.5 text-[11px] font-medium leading-4 tracking-[0.275px] text-emerald-300">
-                      Beta
-                    </span>
+                <DropdownItem key="meeting" classNames={{ wrapper: 'group' }}>
+                  <div className="text-sm font-medium text-zinc-200">
+                    <span className="inline-block">Meeting</span>
                   </div>
-                </div>
-                <div className="text-xs text-zinc-400 group-hover:text-zinc-200">
-                  Host sessions with large audiences
-                </div>
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
+                  <div className="text-xs text-zinc-400 group-hover:text-zinc-200">
+                    Personal or group meetings
+                  </div>
+                </DropdownItem>
+                <DropdownItem key="event" classNames={{ wrapper: 'group' }}>
+                  <div className="flex justify-between text-sm font-medium text-zinc-200">
+                    <span className="inline-block">Webinar</span>
+                    <div className="inline-flex items-center">
+                      <span className="rounded-sm border-1 border-emerald-800 bg-emerald-950 px-1.5 text-[11px] font-medium leading-4 tracking-[0.275px] text-emerald-300">
+                        Beta
+                      </span>
+                    </div>
+                  </div>
+                  <div className="text-xs text-zinc-400 group-hover:text-zinc-200">
+                    Host sessions with large audiences
+                  </div>
+                </DropdownItem>
+                <DropdownItem key="schedule" classNames={{ wrapper: 'group' }}>
+                  <div className="flex justify-between text-sm font-medium text-zinc-200">
+                    <span className="inline-block">Schedule</span>
+                    <div className="inline-flex items-center">
+                      <span className="rounded-sm border-1 border-emerald-800 bg-emerald-950 px-1.5 text-[11px] font-medium leading-4 tracking-[0.275px] text-emerald-300">
+                        Beta
+                      </span>
+                    </div>
+                  </div>
+                  <div className="text-xs text-zinc-400 group-hover:text-zinc-200">
+                    Schedule meeting for later
+                  </div>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+          <div className="mt-8 max-w-xs">
+            <p className="mb-2 text-sm text-zinc-400">
+              Got a room code to join?
+            </p>
+            <JoinRoomField />
+          </div>
         </div>
-        <div className="mt-8 max-w-xs">
-          <p className="mb-2 text-sm text-zinc-400">Got a room code to join?</p>
-          <JoinRoomField />
+        <div className="md:px-5 lg:px-10">
+          <div className="max-w-lg">
+            <MeetingList />
+          </div>
         </div>
       </div>
-      <div className="md:px-5 lg:px-10">
-        <div className="max-w-lg">
-          <MeetingList />
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
---
+++ app/_features/meeting/schedule-modal.tsx
@@ -0,0 +1,52 @@
+import { useCallback, useEffect, type Key } from 'react';
+import {
+  Modal,
+  ModalBody,
+  useDisclosure,
+  ModalContent,
+  ModalHeader,
+} from '@nextui-org/react';
+import MeetingScheduleForm from './schedule-form';
+
+export default function ScheduleModal() {
+  const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure();
+
+  useEffect(() => {
+    const openModal = () => {
+      onOpen();
+    };
+
+    document.addEventListener('open:schedule-meeting-modal', openModal);
+    document.addEventListener('close:schedule-meeting-modal', onClose);
+
+    return () => {
+      document.removeEventListener('open:schedule-meeting-modal', openModal);
+      document.removeEventListener('close:schedule-meeting-modal', onClose);
+    };
+  }, [onClose, onOpen]);
+
+  return (
+    <Modal
+      size="md"
+      placement="top"
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+      isKeyboardDismissDisabled
+      isDismissable={false}
+      hideCloseButton
+      scrollBehavior="inside"
+    >
+      <ModalContent className="p-2">
+        <ModalHeader className="flex flex-col">
+          <h2>Schedule a Meeting</h2>
+          <p className="text-sm font-normal text-zinc-400">
+            Send a personal email to schedule a meeting
+          </p>
+        </ModalHeader>
+        <ModalBody>
+          <MeetingScheduleForm></MeetingScheduleForm>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
