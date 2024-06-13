7da04bca6831802294719c3f996e24a1db695b14
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-06-13 13:45:15 +0700

    feat: update wanna build a project section content

---
+++ app/_shared/components/icons/tabler-external-icon.tsx
@@ -0,0 +1,20 @@
+import type { SVGElementPropsType } from '@/_shared/types/types';
+
+export default function TablerArrowRightIcon(props: SVGElementPropsType) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+      />
+    </svg>
+  );
+}
