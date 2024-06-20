cfc20ba1e04dde76f8ae310f4b00f3f41259b19a
gagahgk <20636513+GagahGK@users.noreply.github.com>
2024-06-20 17:12:58 +0700

    fix: room table should have explicit fk to user table

--- app/(server)/_features/room/schema.ts
+++ app/(server)/_features/room/schema.ts
@@ -1,10 +1,13 @@
 import jsonb from '@/(server)/_shared/database/custom-type';
 import { text, integer, pgTable, timestamp } from 'drizzle-orm/pg-core';
+import { users } from '../user/schema';
 
 export const rooms = pgTable('rooms', {
   id: text('id').notNull().primaryKey(), //this refer to inlive room room-id, room.inlive.app/<id> and also the hubID
   name: text('name'),
-  createdBy: integer('created_by').notNull(),
+  createdBy: integer('created_by')
+    .notNull()
+    .references(() => users.id),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   meta: jsonb('meta'),
 });
