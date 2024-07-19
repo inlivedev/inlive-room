23c712e369ba29d557cecab65433bce016d13d3f
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-07-19 13:54:14 +0700

    chore: update inlive-js-sdk to 0.19.1 (#244)
    * chore: update inlive-js-sdk to 0.19.1
    
    * chore: update drizzle kit to 0.23.0 version
    
    * feat: use create auth module for sdk

--- app/(server)/_shared/utils/sdk.ts
+++ app/(server)/_shared/utils/sdk.ts
@@ -1,13 +1,32 @@
-import { Room } from '@inlivedev/inlive-js-sdk/dist/room';
-
+import { Room, createAuth } from '@inlivedev/inlive-js-sdk/dist/room';
+const inliveHubApiKey = process.env.INLIVE_HUB_API_KEY;
+const inliveApiOrigin = process.env.NEXT_PUBLIC_INLIVE_API_ORIGIN;
+const inliveApiVersion = process.env.NEXT_PUBLIC_INLIVE_API_VERSION;
 const inliveHubOrigin = process.env.NEXT_PUBLIC_INLIVE_HUB_ORIGIN;
 const inliveHubVersion = process.env.NEXT_PUBLIC_INLIVE_HUB_VERSION;
-const inliveHubApiKey = process.env.INLIVE_HUB_API_KEY;
 
-export const serverSDK = Room({
+const createSDKAuth = async () => {
+  if (typeof window !== 'undefined') return null;
+
+  return createAuth({
+    apiVersion: inliveApiVersion,
+    baseUrl: inliveApiOrigin,
+    apiKey: inliveHubApiKey,
+    expirySeconds: 3600,
+  });
+};
+
+const serverSDK = Room({
   api: {
     baseUrl: inliveHubOrigin,
     version: inliveHubVersion,
-    apiKey: inliveHubApiKey,
   },
 });
+
+const sdkAuth = await createSDKAuth();
+
+if (sdkAuth) {
+  serverSDK.setAuth(sdkAuth);
+}
+
+export { serverSDK };
