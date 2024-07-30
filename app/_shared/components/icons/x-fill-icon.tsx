89ebf945b6549481a4c4646688b4300fd51fd20e
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-07-30 14:11:35 +0700

    fix: change icons

--- app/_shared/components/icons/x-fill-icon.tsx
+++ app/_shared/components/icons/x-fill-icon.tsx
@@ -2,10 +2,15 @@ import type { SVGElementPropsType } from '@/_shared/types/types';
 
 export default function XFillIcon(props: SVGElementPropsType) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12" {...props}>
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
       <path
+        d="m4.21 4.387.083-.094a1 1 0 0 1 1.32-.083l.094.083L12 10.585l6.293-6.292a1 1 0 1 1 1.414 1.414L13.415 12l6.292 6.293a1 1 0 0 1 .083 1.32l-.083.094a1 1 0 0 1-1.32.083l-.094-.083L12 13.415l-6.293 6.292a1 1 0 0 1-1.414-1.414L10.585 12 4.293 5.707a1 1 0 0 1-.083-1.32l.083-.094-.083.094z"
         fill="currentColor"
-        d="m1.897 2.054l.073-.084a.75.75 0 0 1 .976-.073l.084.073L6 4.939l2.97-2.97a.75.75 0 1 1 1.06 1.061L7.061 6l2.97 2.97a.75.75 0 0 1 .072.976l-.073.084a.75.75 0 0 1-.976.073l-.084-.073L6 7.061l-2.97 2.97A.75.75 0 1 1 1.97 8.97L4.939 6l-2.97-2.97a.75.75 0 0 1-.072-.976l.073-.084l-.073.084Z"
       />
     </svg>
   );
