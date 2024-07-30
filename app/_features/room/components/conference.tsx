05f8b7591ca1b8db52d23bb7e3187439e55f3e71
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-07-30 16:16:17 +0700

    fix: update sidebar and remove use only meeting layout

--- app/_features/room/components/conference.tsx
+++ app/_features/room/components/conference.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useDisclosure } from '@nextui-org/react';
 import ConferenceTopBar from '@/_features/room/components/conference-top-bar';
 import ConferenceActionsBar from '@/_features/room/components/conference-actions-bar';
 import {
@@ -12,94 +11,79 @@ import { useMetadataContext } from '@/_features/room/contexts/metadata-context';
 import SpotlightView from './spotlight-view';
 import MeetingPresentationLayout from './meeting-presentation-layout';
 import GalleryLayout from './gallery-layout';
-import WebinarSpeakerLayout from './webinar-speaker-layout';
-import WebinarPresentationLayout from './webinar-presentation-layout';
 import HiddenView from './hidden-view';
 import ConferenceNotification from './ conference-notification';
-import ParticipantListMenu from './participant-list-menu';
-import RightDrawerMenu from './right-drawer-menu';
+import ParticipantListMenu from './participant-list-sidebar';
+import RightSidebar from './right-sidebar';
 
-export default function Conference({ roomType }: { roomType: string }) {
+export type Sidebar = 'participants' | 'chat' | '';
+
+export default function Conference() {
+  const [sidebar, setSidebar] = useState<Sidebar>('');
+
+  useEffect(() => {
+    const openRightDrawer = ((event: CustomEventInit) => {
+      setSidebar(event.detail?.menu || '');
+    }) as EventListener;
+
+    const closeRightDrawer = (() => setSidebar('')) as EventListener;
+
+    document.addEventListener('open:right-drawer-menu', openRightDrawer);
+    document.addEventListener('close:right-drawer-menu', closeRightDrawer);
+
+    return () => {
+      document.removeEventListener('open:right-drawer-menu', openRightDrawer);
+      document.removeEventListener('close:right-drawer-menu', closeRightDrawer);
+    };
+  }, []);
+
   return (
     <div className="viewport-height grid grid-cols-[1fr,auto]">
-      <div className="relative grid h-full grid-rows-[auto,auto,1fr,72px] overflow-y-hidden">
-        <ConferenceTopBar />
-        <ParticipantView roomType={roomType} />
-        <div>
-          <ConferenceActionsBar />
-        </div>
+      <div className="relative grid h-full grid-rows-[auto,1fr,72px] overflow-y-hidden">
+        <ConferenceTopBar sidebar={sidebar} />
+        <ConferenceParticipants sidebar={sidebar} />
+        <ConferenceActionsBar />
       </div>
-      <RightDrawerMenuContainer />
     </div>
   );
 }
 
-const RightDrawerMenuContainer = () => {
-  type Menu = 'participants' | 'chat' | '';
-  const [menu, setMenu] = useState<Menu>('');
-  const { isOpen, onOpenChange, onOpen, onClose } = useDisclosure();
-
-  useEffect(() => {
-    const openRightDrawerMenu = ((event: CustomEventInit) => {
-      const { menu } = event.detail || {};
-      setMenu(menu);
-      onOpen();
-    }) as EventListener;
-
-    const closeRightDrawerMenu = (() => {
-      setMenu('');
-      onClose();
-    }) as EventListener;
-
-    document.addEventListener('open:right-drawer-menu', openRightDrawerMenu);
-    document.addEventListener('close:right-drawer-menu', closeRightDrawerMenu);
-
-    return () => {
-      document.removeEventListener(
-        'open:right-drawer-menu',
-        openRightDrawerMenu
-      );
-      document.removeEventListener(
-        'close:right-drawer-menu',
-        closeRightDrawerMenu
-      );
-    };
-  }, [onOpen, onClose]);
-
-  return (
-    <RightDrawerMenu isOpen={isOpen} onOpenChange={onOpenChange}>
-      {menu === 'participants' ? <ParticipantListMenu /> : null}
-    </RightDrawerMenu>
-  );
-};
-
-const ParticipantView = ({ roomType }: { roomType: string }) => {
+const ConferenceParticipants = ({ sidebar }: { sidebar: Sidebar }) => {
   const { streams } = useParticipantContext();
   const pinnedStream =
     streams[0]?.pin || streams[0]?.spotlight ? streams[0] : undefined;
 
   return (
-    <>
-      <div className="px-4">
-        {pinnedStream?.spotlight && pinnedStream?.origin === 'local' ? (
-          <ConferenceNotification
-            show={true}
-            text="You are currently being spotlighted. Your video is highlighted for everyone."
-          />
+    <div className="px-4">
+      <div className="relative grid h-full w-full grid-cols-[auto,minmax(auto,max-content)]">
+        <div className="grid grid-rows-[auto,1fr]">
+          <div>
+            {pinnedStream?.spotlight && pinnedStream?.origin === 'local' ? (
+              <div className="mb-3">
+                <ConferenceNotification
+                  show={true}
+                  text="You are currently being spotlighted. Your video is highlighted for everyone."
+                />
+              </div>
+            ) : null}
+          </div>
+          <div>
+            <ParticipantLayout streams={streams} pinnedStream={pinnedStream} />
+          </div>
+        </div>
+        {sidebar ? (
+          <div className="ml-4 w-[360px]">
+            <RightSidebar isOpen={!!sidebar}>
+              {sidebar === 'participants' ? <ParticipantListMenu /> : null}
+            </RightSidebar>
+          </div>
         ) : null}
       </div>
-      <div className="mt-3 px-4 pb-4">
-        {roomType === 'event' ? (
-          <WebinarView streams={streams} pinnedStream={pinnedStream} />
-        ) : (
-          <MeetingView streams={streams} pinnedStream={pinnedStream} />
-        )}
-      </div>
-    </>
+    </div>
   );
 };
 
-const MeetingView = ({
+const ParticipantLayout = ({
   streams,
   pinnedStream,
 }: {
@@ -145,52 +129,6 @@ const MeetingView = ({
 
   if (currentLayout === 'presentation') {
     return <MeetingPresentationLayout streams={streams} />;
-  }
-
-  return <GalleryLayout streams={streams} />;
-};
-
-const WebinarView = ({
-  streams,
-  pinnedStream,
-}: {
-  streams: ParticipantVideo[];
-  pinnedStream: ParticipantVideo | undefined;
-}) => {
-  const { currentLayout } = useMetadataContext();
-
-  if (pinnedStream) {
-    const hiddenStreams = streams.slice(1);
-
-    if (pinnedStream.origin === 'local') {
-      return (
-        <>
-          <SpotlightView streamA={pinnedStream} />
-          <HiddenView streams={hiddenStreams} />
-        </>
-      );
-    }
-
-    const localStreamIndex = hiddenStreams.findIndex((stream) => {
-      return stream.origin === 'local' && stream.source === 'media';
-    });
-
-    const localStream = hiddenStreams.splice(localStreamIndex, 1)[0];
-
-    return (
-      <>
-        <SpotlightView streamA={pinnedStream} streamB={localStream} />
-        <HiddenView streams={hiddenStreams} />
-      </>
-    );
-  }
-
-  if (currentLayout === 'presentation') {
-    return <WebinarPresentationLayout streams={streams} />;
-  }
-
-  if (currentLayout === 'speaker') {
-    return <WebinarSpeakerLayout streams={streams} />;
   }
 
   return <GalleryLayout streams={streams} />;
