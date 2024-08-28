c4876def18a9d2e510ec1f62bd68ede9bfe2b9bc
Yohan Totting <m@tyohan.dev>
2024-08-28 13:58:20 +0700

    revert sentry config

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,16 +1,13 @@
-// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
-// The config you add here will be used whenever one of the edge features is loaded.
-// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+import * as Sentry from '@sentry/nextjs';
 
-import * as Sentry from "@sentry/nextjs";
+const APP_ENV = process.env.NEXT_PUBLIC_APP_ENV;
+const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init({
-  dsn: "https://b83caf99a22cef8398c1648c08d3e847@o981895.ingest.us.sentry.io/4505683875856384",
-
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+if (SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    environment: APP_ENV,
+    debug: false,
+    tracesSampleRate: 0.1,
+  });
+}
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,19 +1,13 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+import * as Sentry from '@sentry/nextjs';
 
-import * as Sentry from "@sentry/nextjs";
+const APP_ENV = process.env.NEXT_PUBLIC_APP_ENV;
+const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init({
-  dsn: "https://b83caf99a22cef8398c1648c08d3e847@o981895.ingest.us.sentry.io/4505683875856384",
-
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-  
-});
+if (SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    environment: APP_ENV,
+    debug: false,
+    tracesSampleRate: 0.1,
+  });
+}
