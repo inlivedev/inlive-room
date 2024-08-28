63498d88d8d58930d805bcf250c55b35e7bae6f5
Yohan Totting <m@tyohan.dev>
2024-08-28 14:12:46 +0700

    fix build

--- app/(server)/_shared/utils/generateid.ts
+++ app/(server)/_shared/utils/generateid.ts
@@ -1,9 +1,10 @@
 import { customAlphabet } from 'nanoid';
 import { nolookalikesSafe } from 'nanoid-dictionary';
 
-export const generateID = (lenght?: number): (() => string) => {
+export const generateID = (lenght?: number) => {
   if (lenght === undefined) {
     lenght = 12;
   }
-  return customAlphabet(nolookalikesSafe, lenght);
+  const nanoid = customAlphabet(nolookalikesSafe, lenght);
+  return nanoid();
 };
