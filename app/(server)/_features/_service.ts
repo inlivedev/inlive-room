e41c3be5e33172da73d4ed6907a1c782303cb3c3
gagahgk <20636513+GagahGK@users.noreply.github.com>
2024-10-01 14:18:18 +0700

    refactor: ServiceError now has original error as aditional parameter

--- app/(server)/_features/_service.ts
+++ app/(server)/_features/_service.ts
@@ -1,9 +1,16 @@
 export class ServiceError extends Error {
   code: number;
+  originalError?: Error;
 
-  constructor(name = 'ServiceError', message: string, errorCode: number) {
+  constructor(
+    name = 'ServiceError',
+    message: string,
+    errorCode: number,
+    originalError?: Error
+  ) {
     super(message);
     this.name = name;
     this.code = errorCode;
+    this.originalError = originalError;
   }
 }
