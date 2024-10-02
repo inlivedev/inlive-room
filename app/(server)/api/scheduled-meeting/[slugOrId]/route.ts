8657a0d87bc948bbb61a53d9b6f3256e4f2b3334
gagahgk <20636513+GagahGK@users.noreply.github.com>
2024-10-02 10:34:35 +0700

    feat: cancel scheduled meeting endpoint

--- app/(server)/api/scheduled-meeting/[slugOrId]/route.ts
+++ app/(server)/api/scheduled-meeting/[slugOrId]/route.ts
@@ -169,3 +169,109 @@ export async function PUT(
     );
   }
 }
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { slugOrId: string } }
+) {
+  if (!ENABLE_EDIT_MEETING) {
+    return NextResponse.json(
+      {
+        code: 400,
+        message: 'feature not enabled',
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const slugOrID = decodeURIComponent(params.slugOrId);
+  const cookieStore = cookies();
+  const requestToken = cookieStore.get('token');
+
+  if (!requestToken) {
+    return NextResponse.json(
+      {
+        code: 401,
+        message: 'Please check if token is provided in the cookie',
+      },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const response = await getCurrentAuthenticated(requestToken?.value || '');
+    const user = response.data ? response.data : null;
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          code: 401,
+          ok: false,
+          message:
+            'User not found, please check if token is provided in the cookie is valid',
+        },
+        { status: 401 }
+      );
+    }
+
+    if (!user.accountId) {
+      return NextResponse.json(
+        {
+          code: 400,
+          ok: false,
+          message: 'Feature not enabled',
+        },
+        { status: 401 }
+      );
+    }
+
+    const deletedEvent = await scheduledMeetingService.cancelScheduledMeeting(
+      slugOrID,
+      user
+    );
+
+    if (!deletedEvent) {
+      return NextResponse.json(
+        {
+          code: 404,
+          message: 'Meeting not found',
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        code: 204,
+        ok: true,
+        data: deletedEvent,
+        message: 'success',
+      },
+      { status: 204 }
+    );
+  } catch (e) {
+    defaultLogger.captureException(e);
+
+    if (e instanceof z.ZodError) {
+      return NextResponse.json(
+        {
+          code: 400,
+          ok: false,
+          message: e.errors,
+        },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        code: 500,
+        message: 'internal server error',
+        ok: false,
+      },
+      { status: 500 }
+    );
+  }
+}
